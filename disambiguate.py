cb90f988486df33a580a56904510623bd289d027
Megan Leet <megan.leet@iinet.net.au>
2018-07-09 16:58:07 +1000

    Add name disambiguation.

---
+++ disambiguate.py
@@ -0,0 +1,145 @@
+import configparser
+import os
+import sys
+import re
+import itertools
+import collections
+
+import config
+
+debug = False
+
+#TODO Add X-Ambiguous-Name and X-Disambiguator fields (or something named a bit
+#better)
+def convert_desktop(path):
+	parser = configparser.ConfigParser(interpolation=None)
+	parser.optionxform = str #Can you actually fuck off?
+	parser.read(path)
+	return {section: {k: v for k, v in parser.items(section)} for section in parser.sections()}
+
+def get_field(desktop, name):
+	entry = desktop['Desktop Entry']
+	if name in entry:
+		return entry[name]
+	else:
+		return None
+
+def get_array(desktop, name):
+	field = get_field(desktop, name)
+	if field is None:
+		return []
+	else:
+		return field.split(';')
+
+def update_name(desktop, new_name):
+	desktop[1]['Desktop Entry']['Name'] = new_name
+	writer = configparser.ConfigParser(interpolation=None)
+	writer.optionxform = str #You don't fucking understand .desktop files are case sensitive you're fucking
+                          #them up by fucking up the case for fucks sake stop it who asked you to do
+                          #that you stupid fuckwit
+	writer.read_dict(desktop[1])
+	with open(desktop[0], 'wt') as f:
+		writer.write(f)
+		
+def resolve_duplicates_by_platform(group):
+	platform_counter = collections.Counter(get_field(d[1], 'X-Platform') for d in group)
+	for dup in group:
+		platform = get_field(dup[1], 'X-Platform')
+		name = get_field(dup[1], 'Name')
+			
+		#See if this launcher is unique in this group (of launchers with the same
+		#name) for its platform.  If it's the only launcher for this platform, we can
+		#use Thing (Platform) as the final display name to avoid ambiguity
+		if platform_counter[platform] == 1:
+			if platform != name and platform is not None:
+				#Avoid "Doom (Doom)" and "Blah (None)" and such
+				update_name(dup, '{0} ({1})'.format(name, platform))
+		else:
+			if platform_counter[platform] == len(group):
+				#Platform is the same across the whole group.  Need to disambiguate some
+				#other way
+				pass
+			else:
+				#Platform is different, but still requires disambiguation.  That is to say,
+				#there is something else with the same platform in this group, but we still
+				#need to append the platform to disambiguate it from other stuff
+				if platform != name and platform is not None:
+					update_name(dup, '{0} ({1})'.format(name, platform))
+				
+def resolve_duplicates_by_filename_tags(group):
+	for dup in group:
+		the_rest = [d for d in group if d[0] != dup[0]]
+		tags = get_array(dup[1], 'X-Filename-Tags')
+		name = get_field(dup[1], 'Name')
+		
+		differentiator_candidates = []
+		
+		rest_tags = [get_array(rest[1], 'X-Filename-Tags') for rest in the_rest]
+		for tag in tags:
+			if not all([tag in rest_tag for rest_tag in rest_tags]):
+				differentiator_candidates.append(tag)
+		
+		if len(differentiator_candidates) > 0:
+			update_name(dup, '{0} {1}'.format(name, ' '.join(differentiator_candidates)))
+		
+def resolve_duplicates_by_extension(group):
+	extension_counter = collections.Counter(get_field(d[1], 'X-Extension') for d in group)
+	for dup in group:
+		ext = get_field(dup[1], 'X-Extension')
+		name = get_field(dup[1], 'Name')
+			
+		if extension_counter[ext] == 1:
+			update_name(dup, '{0} (.{1})'.format(name, ext))
+		else:
+			if extension_counter[ext] == len(group):
+				pass
+			else:
+				update_name(dup, '{0} (.{1})'.format(name, ext))	
+
+def resolve_duplicates(group, method):
+	if method == 'platform':
+		resolve_duplicates_by_platform(group)
+	elif method == 'tags':
+		resolve_duplicates_by_filename_tags(group)
+	elif method == 'extension':
+		resolve_duplicates_by_extension(group)
+
+def normalize_name(n):
+	n = n.lower()
+	n = re.sub(r'\.\B', '', n)
+	n = n.replace('3-D', '3D')
+	n = n.replace('&', 'and')
+	n = n.replace(" 'n", "'n")
+	n = re.sub(r'\b-\b', ' ', n)
+	n = n.replace(': ', ' - ')
+	n = n.replace('é', 'e')
+	n = n.replace('!', '')
+	n = n.replace('Dr. ', 'Dr ')
+	
+	return n
+
+def fix_duplicate_names(method):
+	files = [(path, convert_desktop(path)) for path in [os.path.join(config.output_folder, f) for f in os.listdir(config.output_folder)]]
+
+	keyfunc = lambda f: normalize_name(get_field(f[1], 'Name'))
+	files.sort(key=keyfunc)
+	duplicates = {}
+	for key, group in itertools.groupby(files, key=keyfunc):
+		g = list(group)
+		if len(g) > 1:
+			duplicates[key] = g
+
+	for k, v in duplicates.items():
+		if method == 'check':
+			print('Duplicate name still remains: ', k, [d[1]['Desktop Entry']['Comment'] for d in v])
+		else:
+			resolve_duplicates(v, method)
+	
+def disambiguate_names(_debug):
+	debug = _debug
+	fix_duplicate_names('platform')
+	fix_duplicate_names('tags')
+	fix_duplicate_names('extension')
+	if debug:
+		fix_duplicate_names('check')
+
--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import sys
 import config
 import roms
 import mame_machines
+import disambiguate
 
-#TODO Add X-Ambiguous-Name and X-Disambiguator fields (or something named a bit better)
 #TODO: For floppy based systems that use MAME (well, currently using MAME for all computers) and have more than one floppy drive, for multi-disk games insert all the disks of the game all at once. I _think_ that works anyway, at least where number_of_disks <= number_of_drives which seems to be the case most of the time. This would be tricky though - I guess I'd assume disk 1 is the boot disk, and other disks are not, and build command lines programmatically. Even if I don't do this, maybe I should only create a launcher for disk 1?
 
 
@@ -37,6 +37,11 @@ for emulator in config.emulator_configs:
 	roms.process_emulator(emulator, debug)
 	time_ended = time.perf_counter()
 	print(emulator['name'], 'finished in', str(datetime.timedelta(seconds=time_ended - time_started)))
+
+time_started = time.perf_counter()
+disambiguate.disambiguate_names(debug)
+time_ended = time.perf_counter()
+print('Name disambiguation finished in', str(datetime.timedelta(seconds=time_ended - time_started)))
 
 
 overall_time_ended = time.perf_counter()
