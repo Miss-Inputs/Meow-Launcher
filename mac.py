0474c09afb122285c03d784ca6666606f6d1bac7
Megan Leet <megan.leet@iinet.net.au>
2018-07-14 00:42:20 +1000

    I'm officially a dickhead

--- mac.py
+++ mac.py
@@ -58,7 +58,7 @@ def make_launcher(path, game_name, game_config):
 	#Or controls... but I swear I will find a way!!!!
 	
 	#If you're not using an SDL2 build of BasiliskII, you probably want to change dga to window! Well you really want to get an SDL2 build of BasiliskII, honestly
-	actual_emulator_command = 'BasiliskII --screen dga/{2}/{3}'.format(width, height)
+	actual_emulator_command = 'BasiliskII --screen dga/{0}/{1}'.format(width, height)
 	inner_command = 'echo {0} > {1} && {2} && rm {1}'.format(shlex.quote(path), shlex.quote(autoboot_txt_path), actual_emulator_command)
 	command = 'sh -c {0}'.format(shlex.quote(inner_command))
 	display_name = common.remove_filename_tags(game_name)
