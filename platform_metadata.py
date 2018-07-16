466178431115cdd818a8741400551ccd7c2586f2
Megan Leet <megan.leet@iinet.net.au>
2018-07-16 20:31:39 +1000

    Oh dear, apparently copyright is a builtin.

--- platform_metadata.py
+++ platform_metadata.py
@@ -235,8 +235,8 @@ def add_ds_metadata(game):
 def add_ngp_metadata(game):
 	game.metadata.tv_type = TVSystem.Agnostic
 	
-	copyright = game.rom.read(amount=28).decode('ascii', errors='backslashreplace')
-	if copyright == 'COPYRIGHT BY SNK CORPORATION':
+	copyright_string = game.rom.read(amount=28).decode('ascii', errors='ignore')
+	if copyright_string == 'COPYRIGHT BY SNK CORPORATION':
 		game.metadata.author = 'SNK'
 	#Otherwise it'd say " LICENSED BY SNK CORPORATION" and that could be any dang third party which isn't terribly useful
 	#There's really not much here, so I didn't even bother reading the whole header
