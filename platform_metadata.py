ad4d3c95bdc2b90fae00f5b55b45437293dee3cd
Megan Leet <megan.leet@iinet.net.au>
2018-07-17 14:06:45 +1000

    platform_metadata: Use Stella's in-built database to get Atari 2600 metadata.

--- platform_metadata.py
+++ platform_metadata.py
@@ -2,6 +2,8 @@ import sys
 import os
 import xml.etree.ElementTree as ElementTree
 import binascii
+import subprocess
+import hashlib
 
 from region_info import TVSystem
 from metadata import SaveType
@@ -651,6 +653,113 @@ def add_pokemini_metadata(game):
 		game.metadata.specific_info['Product-Code'] = product_code
 	except UnicodeDecodeError:
 		pass
+
+def get_stella_database():
+	proc = subprocess.run(['stella', '-listrominfo'], stdout=subprocess.PIPE, universal_newlines=True)
+	proc.check_returncode()
+	
+	lines = proc.stdout.splitlines()
+	first_line = lines[0]
+	lines = lines[1:]
+
+	columns = {}
+	column_names = first_line.split('|')
+	for i in range(0, len(column_names)):
+		columns[i] = column_names[i]
+	
+	games = {}
+	for line in lines:
+		game_columns = line.split('|')
+		game = {}
+
+		md5 = None
+		for i in range(0, len(game_columns)):
+			if i in columns:
+				if columns[i] == 'Cartridge_MD5':
+					md5 = game_columns[i].lower()
+				elif game_columns[i]:
+					game[columns[i]] = game_columns[i]
+	
+		if md5:
+			games[md5] = game
+
+	return games
+
+stella_input_methods = {
+	'JOYSTICK': 'Normal',
+	'PADDLES': 'Paddle',
+	'PADDLES_IAXIS': 'Paddle',
+	'PADDLES_IAXDR': 'Paddle',	
+	'BOOSTERGRIP': 'Normal',
+	'DRIVING': 'Steering Wheel',
+	'KEYBOARD': 'Keyboard',
+	'AMIGAMOUSE': 'Mouse',
+	'ATARIMOUSE': 'Mouse',
+	'TRAKBALL': 'Trackball',
+	'GENESIS': 'Normal',
+	'COMPUMATE': 'Keyboard', #Kinda, it's an entire goddamn computer
+}	
+
+_stella_db = None
+def add_atari_2600_metadata(game):
+	global _stella_db 
+	#Python, you're officially a fucking dumbarse. Of course that's a fucking global variable. It is right there. Two lines above here. In the global fucking scope.
+	if _stella_db is None:
+		try:
+			_stella_db = get_stella_database()
+		except subprocess.CalledProcessError:
+			pass
+	
+	entire_rom = game.rom.read()
+	md5 = hashlib.md5(entire_rom).hexdigest().lower()
+	if md5 in _stella_db:
+		game_info = _stella_db[md5]
+	
+		#TODO: Get year out of name	
+		if 'Cartridge_Manufacturer' in game_info:
+			#TODO: Includes the programmer as well, which is nice, but inconsistent with how X-Author is used elsewhere, maybe split by ', '?
+			game.metadata.author = game_info['Cartridge_Manufacturer']
+		if 'Cartridge_ModelNo' in game_info:
+			game.metadata.specific_info['Product-Code'] = game_info['Cartridge_ModelNo']
+		if 'Cartridge_Note' in game_info:
+			#TODO: Ignore things like "Uses the Paddle Controllers" and "Console ports are swapped" that are already specified by other fields
+			#TODO: Do something with "AKA blah blah blah" (X-Alternate-Name?) or ignore that
+			game.metadata.specific_info['Notes'] = game_info['Cartridge_Note']
+		if 'Controller_Left' in game_info:
+			#TODO: Take note of Controller_SwapPorts or Controller_SwapPaddles and also the right controller
+			#TODO: Use some attribute to note if PADDLES_IAXIS or PADDLES_IAXDR, whatever those do exactly that's different from just PADDLES
+			#There's also things like the Track & Field controller that Stella just calls JOYSTICK so meh
+			left_controller = game_info['Controller_Left']
+			if left_controller in stella_input_methods:
+				game.metadata.input_method = stella_input_methods[left_controller]
+			else:
+				#Includes MindLink because I dunno what to call that
+				game.metadata.input_method = 'Custom'
+
+			if left_controller == 'GENESIS':
+				game.metadata.specific_info['Uses-Genesis-Controller'] = True
+			elif left_controller == 'BOOSTERGRIP':
+				game.metadata.specific_info['Uses-Boostergrip'] = True
+			#TODO: Should differentiate between AMIGAMOUSE and ATARIMOUSE? Maybe that's needed for something	
+	
+		if 'Controller_Right' in game_info:
+			right_controller = game_info['Controller_Right']
+			#Really should be detecting this the same way as the left controller, but for now that's confusing and I'll figure it out later, so this just detects the presence of various peripherals.
+			if right_controller in ('ATARIVOX', 'SAVEKEY'):
+				game.metadata.save_type = SaveType.MemoryCard
+			else:
+				game.metadata.save_type = SaveType.Nothing
+
+			if right_controller == 'KIDVID':
+				game.metadata.specific_info['Uses-Kid-Vid'] = True
+		
+		if 'Display_Format' in game_info:
+			display_format = game_info['Display_Format']
+			if display_format == 'NTSC':
+				game.metadata.tv_type = TVSystem.NTSC
+			elif display_format == 'PAL':
+				game.metadata_tv_type = TVSystem.PAL
+			#TODO: Can also be SECAM, NTSC50, PAL60, or SECAM60
 	
 def nothing_interesting(game):
 	game.metadata.tv_type = TVSystem.Agnostic
@@ -660,6 +769,7 @@ def nothing_interesting(game):
 helpers = {
 	'32X': add_megadrive_metadata,
 	'3DS': add_3ds_metadata,
+	'Atari 2600': add_atari_2600_metadata,
 	'Atari 7800': add_atari7800_metadata,
 	'Atari 8-bit': add_atari_8bit_metadata,
 	'C64': add_commodore_64_metadata,
