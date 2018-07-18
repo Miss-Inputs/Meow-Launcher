de3dc50202b8212b3a8776e75e659f0c91ecc56a
Megan Leet <megan.leet@iinet.net.au>
2018-07-19 00:19:39 +1000

    vectrex.py: Only add year if alphanumeric.

--- platform_metadata/vectrex.py
+++ platform_metadata/vectrex.py
@@ -1,7 +1,11 @@
 from info.region_info import TVSystem
+from common import convert_alphanumeric, NotAlphanumericException
 
 def add_vectrex_metadata(game):
 	game.metadata.tv_type = TVSystem.Agnostic
 	game.metadata.input_method = 'Normal'
-
-	game.metadata.year = game.rom.read(seek_to=6, amount=4).decode('ascii', errors='ignore')
+	
+	try:
+		game.metadata.year = convert_alphanumeric(game.rom.read(seek_to=6, amount=4))
+	except NotAlphanumericException:
+		pass
