29f56824fcadb353a1014dc122f2b86db116be90
Megan Leet <megan.leet@iinet.net.au>
2018-07-18 14:30:44 +1000

    platform_metadata: Fix product codes for Pokemon Mini games. It was just getting the "NINT" out of the copyright string.

--- platform_metadata.py
+++ platform_metadata.py
@@ -748,7 +748,7 @@ def add_pokemini_metadata(game):
 	game.metadata.input_method = 'Normal'
 	
 	#There really isn't much else here, other than maybe the title. I don't think I can do anything with all those IRQs.
-	product_code_bytes = game.rom.read(seek_to=0x21a4, amount=4)
+	product_code_bytes = game.rom.read(seek_to=0x21ac, amount=4)
 	try:
 		product_code = convert_alphanumeric(product_code_bytes)
 		game.metadata.specific_info['Product-Code'] = product_code
