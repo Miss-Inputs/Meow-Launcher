2ccb4f3c443b507244f5ed228751e193b9b37152
Megan Leet <megan.leet@iinet.net.au>
2018-07-23 05:45:35 +1000

    atari_2600: Use autodetection before getting database values. This has the benefit of getting Stella to do the MD5ing for us.

--- platform_metadata/atari_2600.py
+++ platform_metadata/atari_2600.py
@@ -1,4 +1,3 @@
-import hashlib
 import re
 import subprocess
 
@@ -53,17 +52,72 @@ stella_input_methods = {
 }	
 
 stella_display_format_line_regex = re.compile(r'^\s*Display Format:\s*(PAL|NTSC)\*')
+stella_cart_md5_line_regex = re.compile(r'^\s*Cart MD5:\s*([a-z0-9]{32})')
 def autodetect_from_stella(game):
 	proc = subprocess.run(['stella', '-rominfo', game.rom.path], stdout=subprocess.PIPE, universal_newlines=True)
 	if proc.returncode != 0:
-		return
+		return None
 
+	md5 = None
 	lines = proc.stdout.splitlines()
 	for line in lines:
+		cart_md5_match = stella_cart_md5_line_regex.match(line)
+		if cart_md5_match:
+			md5 = cart_md5_match[1]
+			break
+
 		display_format_match = stella_display_format_line_regex.match(line)
 		if display_format_match:
 			game.tv_type = TVSystem[display_format_match.group(1)]
 		#Can also get bankswitch type from here if needed. Controller 0 and Controller 1 too, but then it's probably better to just get that from the database
+	return md5
+
+def parse_stella_db(game, game_info):
+	#TODO: Get year out of name	
+	if 'Cartridge_Manufacturer' in game_info:
+		#TODO: Includes the programmer as well, which is nice, but inconsistent with how X-Author is used elsewhere, maybe split by ', '?
+		game.metadata.author = game_info['Cartridge_Manufacturer']
+	if 'Cartridge_ModelNo' in game_info:
+		game.metadata.specific_info['Product-Code'] = game_info['Cartridge_ModelNo']
+	if 'Cartridge_Note' in game_info:
+		#TODO: Ignore things like "Uses the Paddle Controllers" and "Console ports are swapped" that are already specified by other fields
+		#TODO: Do something with "AKA blah blah blah" (X-Alternate-Name?) or ignore that
+		game.metadata.specific_info['Notes'] = game_info['Cartridge_Note']
+	if 'Controller_Left' in game_info:
+		#TODO: Take note of Controller_SwapPorts or Controller_SwapPaddles and also the right controller
+		#TODO: Use some attribute to note if PADDLES_IAXIS or PADDLES_IAXDR, whatever those do exactly that's different from just PADDLES
+		#There's also things like the Track & Field controller that Stella just calls JOYSTICK so meh
+		left_controller = game_info['Controller_Left']
+		if left_controller in stella_input_methods:
+			game.metadata.input_method = stella_input_methods[left_controller]
+		else:
+			#Includes MindLink because I dunno what to call that
+			game.metadata.input_method = 'Custom'
+
+		if left_controller == 'GENESIS':
+			game.metadata.specific_info['Uses-Genesis-Controller'] = True
+		elif left_controller == 'BOOSTERGRIP':
+			game.metadata.specific_info['Uses-Boostergrip'] = True
+		#TODO: Should differentiate between AMIGAMOUSE and ATARIMOUSE? Maybe that's needed for something	
+	
+	if 'Controller_Right' in game_info:
+		right_controller = game_info['Controller_Right']
+		#Really should be detecting this the same way as the left controller, but for now that's confusing and I'll figure it out later, so this just detects the presence of various peripherals.
+		if right_controller in ('ATARIVOX', 'SAVEKEY'):
+			game.metadata.save_type = SaveType.MemoryCard
+		else:
+			game.metadata.save_type = SaveType.Nothing
+
+		if right_controller == 'KIDVID':
+			game.metadata.specific_info['Uses-Kid-Vid'] = True
+		
+	if 'Display_Format' in game_info:
+		display_format = game_info['Display_Format']
+		if display_format == 'NTSC':
+			game.metadata.tv_type = TVSystem.NTSC
+		elif display_format == 'PAL':
+			game.metadata_tv_type = TVSystem.PAL
+		#TODO: Can also be SECAM, NTSC50, PAL60, or SECAM60
 
 _stella_db = None
 def add_atari_2600_metadata(game):
@@ -75,60 +129,7 @@ def add_atari_2600_metadata(game):
 		except subprocess.CalledProcessError:
 			pass
 	
-	entire_rom = game.rom.read()
-	md5 = hashlib.md5(entire_rom).hexdigest().lower()
+	md5 = autodetect_from_stella(game)
 	if md5 in _stella_db:
 		game_info = _stella_db[md5]
-	
-		#TODO: Get year out of name	
-		if 'Cartridge_Manufacturer' in game_info:
-			#TODO: Includes the programmer as well, which is nice, but inconsistent with how X-Author is used elsewhere, maybe split by ', '?
-			game.metadata.author = game_info['Cartridge_Manufacturer']
-		if 'Cartridge_ModelNo' in game_info:
-			game.metadata.specific_info['Product-Code'] = game_info['Cartridge_ModelNo']
-		if 'Cartridge_Note' in game_info:
-			#TODO: Ignore things like "Uses the Paddle Controllers" and "Console ports are swapped" that are already specified by other fields
-			#TODO: Do something with "AKA blah blah blah" (X-Alternate-Name?) or ignore that
-			game.metadata.specific_info['Notes'] = game_info['Cartridge_Note']
-		if 'Controller_Left' in game_info:
-			#TODO: Take note of Controller_SwapPorts or Controller_SwapPaddles and also the right controller
-			#TODO: Use some attribute to note if PADDLES_IAXIS or PADDLES_IAXDR, whatever those do exactly that's different from just PADDLES
-			#There's also things like the Track & Field controller that Stella just calls JOYSTICK so meh
-			left_controller = game_info['Controller_Left']
-			if left_controller in stella_input_methods:
-				game.metadata.input_method = stella_input_methods[left_controller]
-			else:
-				#Includes MindLink because I dunno what to call that
-				game.metadata.input_method = 'Custom'
-
-			if left_controller == 'GENESIS':
-				game.metadata.specific_info['Uses-Genesis-Controller'] = True
-			elif left_controller == 'BOOSTERGRIP':
-				game.metadata.specific_info['Uses-Boostergrip'] = True
-			#TODO: Should differentiate between AMIGAMOUSE and ATARIMOUSE? Maybe that's needed for something	
-	
-		if 'Controller_Right' in game_info:
-			right_controller = game_info['Controller_Right']
-			#Really should be detecting this the same way as the left controller, but for now that's confusing and I'll figure it out later, so this just detects the presence of various peripherals.
-			if right_controller in ('ATARIVOX', 'SAVEKEY'):
-				game.metadata.save_type = SaveType.MemoryCard
-			else:
-				game.metadata.save_type = SaveType.Nothing
-
-			if right_controller == 'KIDVID':
-				game.metadata.specific_info['Uses-Kid-Vid'] = True
-		
-		if 'Display_Format' in game_info:
-			display_format = game_info['Display_Format']
-			if display_format == 'NTSC':
-				game.metadata.tv_type = TVSystem.NTSC
-			elif display_format == 'PAL':
-				game.metadata_tv_type = TVSystem.PAL
-			elif display_format == 'AUTO':
-				#Yeah, I guess we're just reading the whole ROM again after already reading it once to get the MD5. Oh well. They aren't too big.
-				#TODO: Actually... what if we run -rominfo first, get MD5 from there, and then if it's in stella_db add more values?
-				autodetect_from_stella(game)
-			#TODO: Can also be SECAM, NTSC50, PAL60, or SECAM60
-	else:
-		autodetect_from_stella(game)
-	
+		parse_stella_db(game, game_info)	
