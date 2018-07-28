ce01cf19c919595bb0528070767e8b42cd209356
Megan Leet <megan.leet@iinet.net.au>
2018-07-29 00:51:24 +1000

    MSX2: Take out floppy drive that we aren't using.

--- info/emulator_info.py
+++ info/emulator_info.py
@@ -377,7 +377,7 @@ emulators = {
 	'MAME (Mega Duck)': MameSystem(make_mame_command_line('megaduck', 'cart'), ['bin']),
 	'MAME (MSX1)': MameSystem(make_mame_command_line('svi738', 'cart1', has_keyboard=True), ['bin', 'rom']),
 	#Note that MSX2 is backwards compatible anyway, so there's not much reason to use this, unless you do have some reason. This model in particular is used because it should be completely in English and if anything goes wrong I'd be able to understand it. I still don't know how disks work (they don't autoboot), or if there's even a consistent command to use to boot them.
-	'MAME (MSX2)': MameSystem(make_mame_command_line('fsa1wsx', 'cart1', has_keyboard=True), ['bin', 'rom']),
+	'MAME (MSX2)': MameSystem(make_mame_command_line('fsa1wsx', 'cart1', {'fdc:0', '""'}, has_keyboard=True), ['bin', 'rom']),
 	#This includes MSX2+ because do you really want me to make those two separate things? Turbo-R doesn't work in MAME though, so that'd have to be its own thing. This model is used just because I looked it up and it seems like the best one, the MSX2/MSX2+ systems in MAME are all in Japanese (the systems were only really released in Japan, after all) so you can't avoid that part. Still don't understand disks.
 	'MAME (Neo Geo CD)': MameSystem(make_mame_command_line('neocdz', 'cdrom'), mame_cdrom_formats),
 	#This is interesting, because this runs alright on toasters, but Neo Geo-based arcade games do not (both being
