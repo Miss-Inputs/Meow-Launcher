70d5cfa43681b7b3ccbc98b956145a8f8eb18646
Megan Leet <megan.leet@iinet.net.au>
2018-07-31 19:48:04 +1000

    SMS/GG metadata: Add official header.

--- platform_metadata/master_system.py
+++ platform_metadata/master_system.py
@@ -1,5 +1,8 @@
 import calendar
 
+from region_detect import get_region_by_name
+from platform_metadata.sega_common import licensee_codes
+
 def decode_bcd(i):
 	if not isinstance(i, int):
 		return (decode_bcd(i[1]) * 100) + decode_bcd(i[0])
@@ -31,9 +34,86 @@ def parse_sdsc_header(game, header):
 			game.metadata.developer = game.rom.read(seek_to=author_offset, amount=255).partition(b'\x00')[0].decode('ascii')
 		except UnicodeDecodeError:
 			pass
+
+class BadSMSHeaderException(Exception):
+	pass
+
+regions = {
+	#Second tuple thing: True if Game Gear, False if SMS
+	#Not sure of the difference between export GG and international GG
+	3: ('Japanese', False),
+	4: ('Export', False),
+	5: ('Japanese', True),
+	6: ('Export', True),
+	7: ('International', True),
+}
+	
+def parse_standard_header(game, base_offset):
+	header_data = {}
+	header = game.rom.read(seek_to=base_offset, amount=16)
+
+	if header[:8] != b'TMR SEGA':
+		raise BadSMSHeaderException('TMR missing')
+
+	#Reserved: 8:10
+	#Checksum: 10:12
+	product_code_hi = header[12:14]
+	product_code_lo = (header[14] & 0xf0) >> 4
+	header_data['Revision'] = header[14] & 0x0f
+
+	product_code = '{0}{1:04}'.format('' if product_code_lo == 0 else product_code_lo, decode_bcd(product_code_hi))
+	header_data['Product code'] = product_code
+
+	region_code = (header[15] & 0xf0) >> 4
+	#ROM size = header[15] & 0x0f, uses lookup table; used for calculating checksum
+	
+	if region_code in regions:
+		header_data['Region'], header_data['Is Game Gear'] = regions[region_code]
+		if header_data['Is Game Gear']:
+			if len(product_code) >= 5:
+				#:-3
+				maker_code = 'T-' + product_code[:-3]
+				if maker_code in licensee_codes:
+					header_data['Publisher'] = licensee_codes[maker_code]
+			else:
+				header_data['Publisher'] = 'Sega'
+	else:
+		raise BadSMSHeaderException('Weird region: ' + str(region_code))
+	
+	return header_data
+
+def try_parse_standard_header(game):
+	rom_size = game.rom.get_size()
+	possible_offsets = [0x1ff0, 0x3ff0, 0x7ff0]
+
+	header_data = None
+	for possible_offset in possible_offsets:
+		if possible_offset >= rom_size:
+			continue
+
+		try:
+			header_data = parse_standard_header(game, possible_offset)
+			break
+		except BadSMSHeaderException:
+			continue
+
+	if header_data:
+		game.metadata.revision = header_data['Revision']
+		game.metadata.specific_info['Product-Code'] = header_data['Product code']
+		if header_data['Region'] == 'Japanese':
+			game.metadata.regions = [get_region_by_name('Japan')]
 		
+		if header_data['Is Game Gear']:
+			game.metadata.platform = 'Game Gear'
+		else:
+			game.metadata.platform = 'Master System'
+
+		if 'Publisher' in header_data:
+			game.metadata.publisher = header_data['Publisher']
 
 def get_sms_metadata(game):
 	sdsc_header = game.rom.read(seek_to=0x7fe0, amount=12)
 	if sdsc_header[:4] == b'SDSC':
 		parse_sdsc_header(game, sdsc_header[4:])
+
+	try_parse_standard_header(game)
