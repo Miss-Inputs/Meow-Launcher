5973dc23a12247a5c5294792fc53a19da5dcc81b
Megan Leet <megan.leet@iinet.net.au>
2018-08-01 14:15:11 +1000

    Change import style inside platform_metadata __init__.
    This gets rid of all those pylint undefined variable errors (although they quite obviously were defined and just fine), so I guess it's the right way to do it.

--- platform_metadata/__init__.py
+++ platform_metadata/__init__.py
@@ -3,27 +3,27 @@ from metadata import PlayerInput, InputType
 
 #I don't know why I have to import things from myself. Why? That sucks.
 #Well, I guess if that's the only thing that sucks, it could be worse... but still, this _really_ sucks. This can't be right, can it? Surely there's some trick that nobody thought to put in documentation
-import platform_metadata._3ds
-import platform_metadata.atari_2600
-import platform_metadata.atari_7800
-import platform_metadata.atari_8_bit
-import platform_metadata.commodore_64
-import platform_metadata.ds
-import platform_metadata.game_boy
-import platform_metadata.gamecube
-import platform_metadata.gba
-import platform_metadata.master_system
-import platform_metadata.megadrive
-import platform_metadata.n64
-import platform_metadata.neo_geo_pocket
-import platform_metadata.nes
-import platform_metadata.pokemon_mini
-import platform_metadata.psp
-import platform_metadata.snes
-import platform_metadata.vectrex
-import platform_metadata.virtual_boy
-import platform_metadata.wii
-import platform_metadata.wonderswan
+from platform_metadata._3ds import add_3ds_metadata
+from platform_metadata.atari_2600 import add_atari_2600_metadata
+from platform_metadata.atari_7800 import add_atari7800_metadata
+from platform_metadata.atari_8_bit import add_atari_8bit_metadata
+from platform_metadata.commodore_64 import add_commodore_64_metadata
+from platform_metadata.ds import add_ds_metadata
+from platform_metadata.game_boy import add_gameboy_metadata
+from platform_metadata.gamecube import add_gamecube_metadata
+from platform_metadata.gba import add_gba_metadata
+from platform_metadata.master_system import get_sms_metadata
+from platform_metadata.megadrive import add_megadrive_metadata
+from platform_metadata.n64 import add_n64_metadata
+from platform_metadata.neo_geo_pocket import add_ngp_metadata
+from platform_metadata.nes import add_nes_metadata
+from platform_metadata.pokemon_mini import add_pokemini_metadata
+from platform_metadata.psp import add_psp_metadata
+from platform_metadata.snes import add_snes_metadata
+from platform_metadata.vectrex import add_vectrex_metadata
+from platform_metadata.virtual_boy import add_virtual_boy_metadata
+from platform_metadata.wii import add_wii_metadata
+from platform_metadata.wonderswan import add_wonderswan_metadata
 
 #For roms.py, gets metadata in ways specific to certain platforms
 #I guess this is duplicating a lot of ROMniscience code, huh? Well, it's my project, and I'll use it for reference for my other project if I want. But I guess there is duplication there. I mean, it's C# and Python, so I can't really combine them directly, but it makes me think... it makes me overthink. That's the best kind of think.
@@ -44,31 +44,31 @@ def nothing_interesting(game):
 	game.metadata.input_info.players.append(player)
 
 helpers = {
-	'32X': megadrive.add_megadrive_metadata,
-	'3DS': _3ds.add_3ds_metadata,
-	'Atari 2600': atari_2600.add_atari_2600_metadata,
-	'Atari 7800': atari_7800.add_atari7800_metadata,
-	'Atari 8-bit': atari_8_bit.add_atari_8bit_metadata,
-	'C64': commodore_64.add_commodore_64_metadata,
-	'DS': ds.add_ds_metadata,
+	'32X': add_megadrive_metadata,
+	'3DS': add_3ds_metadata,
+	'Atari 2600': add_atari_2600_metadata,
+	'Atari 7800': add_atari7800_metadata,
+	'Atari 8-bit': add_atari_8bit_metadata,
+	'C64': add_commodore_64_metadata,
+	'DS': add_ds_metadata,
 	'Epoch Game Pocket Computer': nothing_interesting,
 	'Gamate': nothing_interesting,
-	'Game Boy': game_boy.add_gameboy_metadata,
-	'GameCube': gamecube.add_gamecube_metadata,
-	'Game Gear': master_system.get_sms_metadata,
-	'GBA': gba.add_gba_metadata,
-	'Master System': master_system.get_sms_metadata,
-	'Mega Drive': megadrive.add_megadrive_metadata,
+	'Game Boy': add_gameboy_metadata,
+	'GameCube': add_gamecube_metadata,
+	'Game Gear': get_sms_metadata,
+	'GBA': add_gba_metadata,
+	'Master System': get_sms_metadata,
+	'Mega Drive': add_megadrive_metadata,
 	'Mega Duck': nothing_interesting,
-	'N64': n64.add_n64_metadata,
-	'Neo Geo Pocket': neo_geo_pocket.add_ngp_metadata,
-	'NES': nes.add_nes_metadata,
-	'Pokemon Mini': pokemon_mini.add_pokemini_metadata,
-	'PSP': psp.add_psp_metadata,
-	'SNES': snes.add_snes_metadata,
-	'Vectrex': vectrex.add_vectrex_metadata,
-	'Virtual Boy': virtual_boy.add_virtual_boy_metadata,
+	'N64': add_n64_metadata,
+	'Neo Geo Pocket': add_ngp_metadata,
+	'NES': add_nes_metadata,
+	'Pokemon Mini': add_pokemini_metadata,
+	'PSP': add_psp_metadata,
+	'SNES': add_snes_metadata,
+	'Vectrex': add_vectrex_metadata,
+	'Virtual Boy': add_virtual_boy_metadata,
 	'Watara Supervision': nothing_interesting,
-	'Wii': wii.add_wii_metadata,
-	'WonderSwan': wonderswan.add_wonderswan_metadata,
+	'Wii': add_wii_metadata,
+	'WonderSwan': add_wonderswan_metadata,
 }
