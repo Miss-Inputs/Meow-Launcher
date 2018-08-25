c71f6c6d826f4b03e8d8a5e1f576f86c75113419
Megan Leet <megan.leet@iinet.net.au>
2018-08-25 16:41:24 +1000

    Add input info for Atari Lynx games.

--- platform_metadata/minor_systems.py
+++ platform_metadata/minor_systems.py
@@ -41,3 +41,12 @@ def add_watara_supervision_info(game):
 	player.buttons = 2
 	game.metadata.input_info.players.append(player)
 	game.metadata.input_info.console_buttons = 2
+
+def add_lynx_info(game):
+	#TODO .lnx files should have a header with something in them, so eventually, Lynx will get its own module here
+	game.metadata.tv_type = TVSystem.Agnostic
+	player = PlayerInput()
+	player.inputs = [InputType.Digital]
+	player.buttons = 4 #Option 1, Option 2, A, B; these are flipped so you might think there's 8
+	game.metadata.input_info.players.append(player)
+	game.metadata.input_info.console_buttons = 1 #Pause
