ab8370b456c9e0e1bab2a0c9f9a84ae3cdffae6b
Megan Leet <megan.leet@iinet.net.au>
2018-08-26 11:46:47 +1000

    Add ability to skip header when finding software in software lists.

--- platform_metadata/software_list_info.py
+++ platform_metadata/software_list_info.py
@@ -8,15 +8,19 @@ from mame_helpers import get_software_lists_by_names, find_in_software_lists, co
 #Atari 5200, Colecovision: Try parsing title screen from ROM first
 #Intellivison: .int is actually a custom headered file format I think, so that might be tricky to deal with
 #Neo Geo CD, CD-i, PC Engine CD, PC-FX: Not sure how well that would work with disc images, since MAME uses <diskarea> and chds there
-#Atari 7800, NES, Master System, Pokemon Mini, Neo Geo Pocket, Vectrex and N64 could make use of software lists, but they have their own modules, and the former two have custom headers and the latter would take 5 hours (especially when 7zipped)
+#Atari 7800: Has header, need to skip it
+#NES: Has header, but is stored as split prg/chr inside software lists, need to separate that
+#Neo Geo Pocket, Pokemon Mini, Vectrex: TODO
+#Master System: Hmm, already has a publisher for Game Gear and homebrew... year too
+#N64: Not sure if want, this would take 5 hours especially when 7zipped
 #Uzebox: .uze files have a header, need to get onto those actually
 #ZX Spectrum: All +3 software in MAME is in .ipf format, which seems hardly ever used in all honesty (.z80 files don't have a software list)
 
-def get_software_list_entry(game):
+def get_software_list_entry(game, skip_header=0):
 	software_list_names = get_mame_software_list_names_by_system_name(game.metadata.platform)
 	software_lists = get_software_lists_by_names(software_list_names)
 	
-	crc32 = '{:08x}'.format(zlib.crc32(game.rom.read()))
+	crc32 = '{:08x}'.format(zlib.crc32(game.rom.read(seek_to=skip_header)))
 	return find_in_software_lists(software_lists, crc=crc32)
 
 def get_part_feature(part, name):
