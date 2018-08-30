34f0818630f7572c3b5a276dc0851420a69daedb
Megan Leet <megan.leet@iinet.net.au>
2018-08-30 13:32:01 +1000

    N64 metadata: Detect format, and byteswap accordingly.

--- platform_metadata/n64.py
+++ platform_metadata/n64.py
@@ -1,22 +1,51 @@
+import zlib
+
 from common import convert_alphanumeric, NotAlphanumericException
-from software_list_info import get_software_list_entry
+from software_list_info import find_in_software_lists
+
+def _byteswap(b):
+	byte_array = bytearray(b)
+	for i in range(0, len(byte_array), 2):
+		temp = byte_array[i]
+		byte_array[i] = byte_array[i + 1]
+		byte_array[i + 1] = temp
+	return bytes(byte_array)
 
 def add_n64_metadata(game):
-	header = game.rom.read(amount=64)
-	magic = header[:4]
-	if magic == b'\x80\x37\x12\x40':
-		#Z64
-		#TODO: Detect format and add as metadata. This'll be useful because some emulators won't even try and launch the thing if the first four bytes don't match something expected, even if it is a raw non-swapped dump that just happens to have weird values there
-		#TODO: Check Mupen64Plus's database (/usr/local/share/mupen64plus/mupen64plus.ini). That will take 5 hours because it's indexed by MD5 and not something like internal name or product code, but eh... it has the things. For ROMs it knows about. Hmm. I guess it's all we can do, unless you want to make a whole database of N64 ROMs yourself, young lady.
-		#TODO: If v64, put the endian down, flip it and reverse it
-		try:
-			product_code = convert_alphanumeric(header[59:63])
-			game.metadata.product_code = product_code
-		except NotAlphanumericException:
-			pass
-		game.metadata.revision = header[63]
+	entire_rom = game.rom.read()
+	
+	magic = entire_rom[:4]
 
-	software = get_software_list_entry(game)
+	byte_swap = False
+	header = entire_rom[:64]
+	if magic == b'\x80\x37\x12\x40':
+		game.metadata.specific_info['ROM-Format'] = 'Z64'
+	elif magic == b'\x37\x80\x40\x12':
+		byte_swap = True
+		game.metadata.specific_info['ROM-Format'] = 'V64'
+	else:
+		#TODO: Detect other formats (there are a few homebrews that start with 0x80 0x37 but not 0x12 0x40 after that
+		game.metadata.specific_info['ROM-Format'] = 'Unknown'
+		return
+
+	#TODO: Check Mupen64Plus's database (/usr/local/share/mupen64plus/mupen64plus.ini). That will take 5 hours because it's indexed by MD5 and not something like internal name or product code, but eh... it has the things. For ROMs it knows about. Hmm. I guess it's all we can do, unless you want to make a whole database of N64 ROMs yourself, young lady.
+
+	if byte_swap:
+		header = _byteswap(header)
+	try:
+		product_code = convert_alphanumeric(header[59:63])
+		game.metadata.product_code = product_code
+	except NotAlphanumericException:
+		pass
+	game.metadata.revision = header[63]
+
+	if not byte_swap:
+		entire_rom = _byteswap(entire_rom)
+		#For some reason, MAME uses little endian dumps in its software list at the moment, hence "not byte_swap" which would be wrong otherwise
+
+	rom_crc32 = '{:08x}'.format(zlib.crc32(entire_rom))
+
+	software = find_in_software_lists(game.software_lists, crc=rom_crc32)
 	if software:
 		software.add_generic_info(game)
 		if not game.metadata.product_code:
