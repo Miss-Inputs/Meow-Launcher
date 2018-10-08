c3cf7ce086eb23c1e5420d014da1175d9cb135bb
Megan Leet <megan.leet@iinet.net.au>
2018-10-09 01:03:29 +1100

    blargle flargle

--- mame_metadata.py
+++ mame_metadata.py
@@ -88,7 +88,7 @@ def add_machine_platform(machine):
 		machine.metadata.platform = 'CPS Changer'
 		machine.name = machine.name.replace('CPS Changer, ', '')
 		machine.metadata.media_type = MediaType.Cartridge
-	elif mamchine.name.endswith('(XaviXPORT)'):
+	elif mamechine.name.endswith('(XaviXPORT)'):
 		machine.metadata.platform = 'XaviXPORT'
 		machine.metadata.media_type = MediaType.Cartridge
 	elif category == 'Game Console':
