39d02f0655d969b0efc83faf832685ad867143a4
Megan Leet <megan.leet@iinet.net.au>
2018-10-11 22:46:16 +1100

    Remove this unused import.

--- mame_metadata.py
+++ mame_metadata.py
@@ -8,7 +8,7 @@ from info.system_info import MediaType
 from metadata import EmulationStatus, CPUInfo, ScreenInfo, SaveType
 from region_detect import get_language_by_english_name, get_regions_from_filename_tags
 from common import find_filename_tags
-from mame_helpers import find_main_cpu, consistentify_manufacturer, get_mame_xml
+from mame_helpers import find_main_cpu, consistentify_manufacturer
 
 debug = '--debug' in sys.argv
 
