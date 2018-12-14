d704096f2f79bcf46c9bc5cad5f68bfb97074928
Megan Leet <megan.leet@iinet.net.au>
2018-12-15 02:52:56 +1100

    Wii: Get disc info from WBFS files.

--- platform_metadata/wii.py
+++ platform_metadata/wii.py
@@ -112,12 +112,14 @@ def add_wii_homebrew_metadata(game):
 
 def add_wii_metadata(game):
 	add_wii_system_info(game)
-	if game.rom.extension in ('gcz', 'iso'):
+	if game.rom.extension in ('gcz', 'iso', 'wbfs'):
 		if game.rom.extension == 'gcz':
 			#Can be a format for Wii discs, though not recommended and uncommon
 			header = cd_read.read_gcz(game.rom.path, amount=0x2450)
 		elif game.rom.extension == 'iso':
 			header = game.rom.read(amount=0x2450)
+		elif game.rom.extension == 'wbfs':
+			header = game.rom.read(amount=0x2450, seek_to=0x200)
 
 		add_gamecube_wii_disc_metadata(game, header)
 	elif game.rom.extension == 'wad':
