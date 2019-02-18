6ec4177b69d7713c76494f7f0e227112d272123e
Megan Leet <megan.leet@iinet.net.au>
2019-02-18 19:00:45 +1100

    Begin the  threadening

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,4 +1,5 @@
 import os
+from threading import Thread
 
 import wx
 
@@ -10,12 +11,42 @@ import main
 
 from .gui_generated import MeowLauncherGui
 
+start_work_event_type = wx.NewEventType()
+start_work_event_binder = wx.PyEventBinder(start_work_event_type)
+work_done_event_type = wx.NewEventType()
+work_done_event_binder = wx.PyEventBinder(work_done_event_type)
+
+class StartWorkEvent(wx.PyCommandEvent):
+	def __init__(self):
+		super().__init__(start_work_event_type)
+
+class WorkDoneEvent(wx.PyCommandEvent):
+	def __init__(self):
+		super().__init__(work_done_event_type)
+
+class WorkerThread(Thread):
+	def __init__(self, wx_object, enabled_state):
+		super().__init__()
+		self.wx_object = wx_object
+		self.enabled_state = enabled_state
+
+	def run(self):
+		start_event = StartWorkEvent()
+		wx.PostEvent(self.wx_object, start_event)
+		main.main(*self.enabled_state)
+		done_event = WorkDoneEvent()
+		wx.PostEvent(self.wx_object, done_event)
+
 class MainWindow(MeowLauncherGui):
 	def __init__(self, parent):
 		super().__init__(parent)
 		self.optionsSizer = self.optionsPanel.GetSizer() #wxFormBuilder won't put it as a property of the main window object
 		self.Icon = wx.Icon('gui/icon.png')
 		self.setupStuff()
+
+		self.workerThread = None
+		self.Bind(start_work_event_binder, self.on_worker_started)
+		self.Bind(work_done_event_binder, self.on_worker_done)
 
 	def setupStuff(self):
 		self.setupMainButtons()
@@ -44,7 +75,14 @@ class MainWindow(MeowLauncherGui):
 
 	def okButtonOnButtonClick(self, event):
 		self.loadRuntimeOptions()
-		main.main(self.mameMachineCheckBox.IsChecked(), self.romsCheckBox.IsChecked(), self.dosCheckBox.IsChecked(), self.macCheckBox.IsChecked(), self.scummvmCheckBox.IsChecked(), self.steamCheckBox.IsChecked())
+		self.workerThread = WorkerThread(self, (self.mameMachineCheckBox.IsChecked(), self.romsCheckBox.IsChecked(), self.dosCheckBox.IsChecked(), self.macCheckBox.IsChecked(), self.scummvmCheckBox.IsChecked(), self.steamCheckBox.IsChecked()))
+		self.workerThread.start()
 
 	def exitButtonOnButtonClick(self, event):
 		self.Close()
+
+	def on_worker_started(self, event):
+		self.okButton.Disable()
+
+	def on_worker_done(self, event):
+		self.okButton.Enable()
