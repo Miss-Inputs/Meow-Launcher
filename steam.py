3c5c107b848d06367ea74ea13ec3cf813fd9c4ad
Megan Leet <megan.leet@iinet.net.au>
2019-03-05 16:47:01 +1100

    Update the thing

--- steam.py
+++ steam.py
@@ -222,24 +222,33 @@ def normalize_developer(dev):
 
 	overrides = {
 		#Different spellings/formattings of the same company, but we like being consistent with all the other modules and I also just disagree with yelling I guess
+		'5th Cell Media': '5th Cell',
 		'AGM PLAYISM': 'AGM Playism',
 		'ALICE IN DISSONANCE': 'Alice in Dissonance',
 		'ASTRO PORT': 'Astro Port',
+		'Asylum entertainment': 'Asylum Entertainment',
+		'ATLUS': 'Atlus',
 		'BANDAI NAMCO Entertainment': 'Bandai Namco',
 		'BANDAI NAMCO Studios': 'Bandai Namco', #Appears as developer, the Bandai Namco Entertainment appears as publisher... hmm
 		'Bethesda-Softworks': 'Bethesda',
 		'CD PROJEKT RED': 'CD Projekt Red',
+		'Crytek Studios': 'Crytek',
 		'DIMPS': 'Dimps',
 		'Dingaling Productions': 'Dingaling',
 		'DONTNOD Entertainment': 'Dontnod Entertainment',
+		'FIVE-BN GAMES': 'Five-BN Games',
 		'GATO STUDIO': 'Gato Studio', #Also seen as "Gato Salvaje", although that doesn't transate exactly to the English name
-		'IDEA FACTORY': 'Idea Factory',
+		'IDEA FACTORY ': 'Idea Factory', #Who puts a damn space there
+		'IKEA Communications AB': 'IKEA Communications',
+		'Konami Digital Entertainment': 'Konami',
 		'Lupus Studios Limited': 'Lupus Studios',
 		'QUICKTEQUILA': 'Quicktequila',
+		'Rockin Android': 'Rockin\' Android'
 		'SEGA': 'Sega',
 		'SNK CORPORATION': 'SNK',
 		'SQUARE ENIX': 'Square Enix',
 		'Topware Interactive ACE': 'Topware Interactive',
+		'Warner Bros. Interactive Entertainment': 'Warner Bros',
 		'XSEED GAMES': 'XSeed Games',
 		'YAGER': 'Yager Development',
 		#Coffee Stain Studios = Coffee Stain Publishing? Hmm
