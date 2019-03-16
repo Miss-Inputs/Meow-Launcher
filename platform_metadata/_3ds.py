a1ce572760f7916fc7c194c71ed007b1e11b55c2
Megan Leet <megan.leet@iinet.net.au>
2019-03-16 13:27:35 +1100

    3DS: Move publisher name override list to data folder.

--- platform_metadata/_3ds.py
+++ platform_metadata/_3ds.py
@@ -13,6 +13,7 @@ from metadata import CPUInfo, ScreenInfo, Screen
 from common import convert_alphanumeric, NotAlphanumericException, junk_suffixes
 from common_types import SaveType
 from .nintendo_common import nintendo_licensee_codes
+from data._3ds_publisher_overrides import consistentified_manufacturers
 
 class _3DSRegionCode(Flag):
 	Japan = 1
@@ -26,20 +27,6 @@ class _3DSRegionCode(Flag):
 
 	def __str__(self):
 		return self.name
-
-consistentified_manufacturers = {
-	#When getting publisher from SMDH, sometimes things are formatted in different ways than wanted (e.g. YELLING CASE), this is here to smooth things out
-	#It's why we try to use licensee code instead, but that's not always there on 3DS
-	#For what it's worth, it seems each publisher will format its own name consistently across all games it publishes, it just looks weird compared to how names are formatted elsewhere
-
-	#Atlus USA, Inc. > Atlus USA?
-	'CAPCOM': 'Capcom',
-	'CIRCLE Ent.': 'Circle Entertainment',
-	'SEGA': 'Sega',
-	'Unspecified Author': None, #Homebrew might do this
-	'VD-DEV': 'VD-Dev',
-	'YouTube': 'Google', #Maybe I should change nintendo_common WB to YouTube. Oh well. Either way, this is the YouTube app, and hence Google
-}
 
 def add_3ds_system_info(game):
 	game.metadata.tv_type = TVSystem.Agnostic
