fcb53e5e056fe514c779b876fd592d203735099f
Megan Leet <megan.leet@iinet.net.au>
2019-03-24 19:44:09 +1100

    Atari 8-bit: Add input_info stuff.

--- platform_metadata/atari_8_bit.py
+++ platform_metadata/atari_8_bit.py
@@ -1,3 +1,4 @@
+import input_metadata
 from software_list_info import get_software_list_entry
 from common_types import MediaType
 
@@ -12,21 +13,36 @@ def add_info_from_software_list(game, software):
 	game.metadata.product_code = software.get_info('serial')
 
 	peripheral = software.get_part_feature('peripheral')
-	#TODO Setup input_info I guess:
-	#cx77_touch = Touchscreen (tablet)
-	#cx75_pen = Light Gun (light pen)
-	#koala_pad,koala_pen = Tablet _and_ light pen
-	#trackball = Trackball
-	#lightgun = Light Gun (XEGS only)
 
-	#trackfld = Track & Field controller but is that just a boneless joystick?
-	#Otherwise do we assume joystick and keyboard? Hmm
+	joystick = input_metadata.NormalController()
+	joystick.dpads = 1
+	joystick.face_buttons = 2
+	keyboard = input_metadata.Keyboard()
+	keyboard.keys = 57 #From looking at photos so I may have lost count; XL/XE might have more keys
+
+	if peripheral == 'cx77_touch':
+		#Tablet
+		game.metadata.input_info.add_option(input_metadata.Touchscreen())
+	elif peripheral == 'cx75_pen':
+		#Light pen
+		game.metadata.input_info.add_option(input_metadata.LightGun())
+	elif peripheral == 'koala_pad,koala_pen':
+		#Combination tablet/light pen
+		game.metadata.input_info.add_option([input_metadata.LightGun(), input_metadata.Touchscreen])
+	elif peripheral == 'trackball':
+		game.metadata.input_info.add_option(input_metadata.Trackball())
+	elif peripheral == 'lightgun':
+		#XEGS only
+		game.metadata.input_info.add_option(input_metadata.LightGun())
+	else:
+		#trackfld = Track & Field controller but is that just a spicy joystick?
+		game.metadata.input_info.add_option([joystick, keyboard])
+
 	game.metadata.specific_info['Peripheral'] = peripheral
 
 	game.metadata.specific_info['Notes'] = software.get_info('usage')
 	#To be used with Atari 1400 onboard modem.
 	#3 or 4 player gameplay available only on 400/800 systems
-	#Keyboard overlay was supplied with cartridge
 	#Chalkboard Inc.'s Powerpad Tablet required
 	#Plays music only in PAL
 	#Requires Lower-Silesian Turbo 2000 hardware modification installed in a tape recorder.
@@ -42,6 +58,9 @@ def add_info_from_software_list(game, software):
 	#Personal Peripherals Inc. Super Sketch device required
 	#Modem required (and a working Chemical Bank service, obviously inactive for decades)
 	#You must type 'X=USR(32768)' from the BASIC prompt to initialize it.
+
+	#Meaningless for our purposes:
+	#Keyboard overlay was supplied with cartridge
 
 def add_atari_8bit_metadata(game):
 	if game.metadata.media_type == MediaType.Cartridge:
