4a579e7955d7f02be0d6b349723153b9dcb9beed
Megan Leet <megan.leet@iinet.net.au>
2019-03-30 12:46:22 +1100

    Don't do that for Satellaview because I'm not sure enough about that

--- platform_metadata/snes.py
+++ platform_metadata/snes.py
@@ -343,7 +343,7 @@ def add_snes_metadata(game):
 	software = get_software_list_entry(game)
 	if software:
 		software.add_generic_info(game)
-		if game.metadata.save_type == SaveType.Unknown:
+		if game.metadata.save_type == SaveType.Unknown and game.metadata.platform != 'Satellaview':
 			game.metadata.save_type = SaveType.Cart if software.has_data_area('nvram') else SaveType.Nothing
 		#We can actually get lorom/hirom from feature = slot. Hmm...
 	#Can't get input_info at this point as there's nothing to distinguish stuff that uses mouse/gun/etc
