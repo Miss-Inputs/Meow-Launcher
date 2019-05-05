d0385452d6bdd77ab707bb7eef4876ec568062b3
Megan Leet <megan.leet@iinet.net.au>
2019-05-05 13:50:34 +1000

    Update data/steam_developer_overrides.

--- data/steam_developer_overrides.py
+++ data/steam_developer_overrides.py
@@ -1,52 +1,55 @@
 developer_overrides = {
 	#Different spellings/formattings of the same company, but we like being consistent with all the other modules and I also just disagree with yelling I guess
 	'5th Cell Media': '5th Cell',
-	'AGM PLAYISM': 'AGM Playism',
-	'ALICE IN DISSONANCE': 'Alice in Dissonance',
-	'ASTRO PORT': 'Astro Port',
-	'Asylum entertainment': 'Asylum Entertainment',
-	'ATLUS': 'Atlus',
 	'BANDAI NAMCO Entertainment': 'Bandai Namco',
 	'BANDAI NAMCO Studios': 'Bandai Namco', #Appears as developer, the Bandai Namco Entertainment appears as publisher... hmm
 	'Bethesda Game Studios': 'Bethesda',
 	'Bethesda-Softworks': 'Bethesda',
 	'Bethesda Softworks': 'Bethesda',
-	'CD PROJEKT RED': 'CD Projekt Red',
 	'Crytek Studios': 'Crytek',
 	'D3T Limited': 'd3t',
-	'DIMPS': 'Dimps',
 	'Dingaling Productions': 'Dingaling',
-	'DONTNOD Entertainment': 'Dontnod Entertainment',
-	'FIVE-BN GAMES': 'Five-BN Games',
 	'Forever Entertainment S. A.': 'Forever Entertainment',
-	'GATO STUDIO': 'Gato Studio', #Also seen as "Gato Salvaje", although that doesn't transate exactly to the English name
-	'IDEA FACTORY ': 'Idea Factory', #Who puts a damn space there
 	'IKEA Communications AB': 'IKEA Communications',
 	'Killmonday Games AB': 'Killmonday Games',
 	'KISS ltd': 'KISS',
-	'KITATUS STUDIOS': 'Kitatus Studios',
 	'Konami Digital Entertainment': 'Konami',
 	'Lupus Studios Limited': 'Lupus Studios',
 	'NAMCO BANDAI Games': 'Namco Bandai', #Should be merged with Bandai Namco? Maybe? I don't know anything anymore
 	'NAMCO BANDAI Studio': 'Namco Bandai',
-	'OVERDRIVE': 'Overdrive',
 	'OVERKILL - a Starbreeze Studio.': 'Overkill Software',
-	'QUICKTEQUILA': 'Quicktequila',
 	'Reflections, a Ubisoft Studio': 'Reflections',
 	'Rockin Android': 'Rockin\' Android',
-	'SEGA': 'Sega',
 	'SNK CORPORATION': 'SNK',
-	'SQUARE ENIX': 'Square Enix',
 	'Starbreeze Publishing AB': 'Starbreeze',
 	'Topware Interactive ACE': 'Topware Interactive',
 	'Warner Bros. Interactive Entertainment': 'Warner Bros',
-	'XSEED': 'XSeed',
 	'XSEED Games': 'XSeed',
 	'XSEED GAMES': 'XSeed',
 	'YAGER': 'Yager Development',
 	#Coffee Stain Studios = Coffee Stain Publishing? Hmm
 	#Lazy Monday Games > Lazy Monday?
 	#Epic Games = Epic MegaGames?
+
+	#Capitalization stuff
+	'AGM PLAYISM': 'AGM Playism',
+	'ALICE IN DISSONANCE': 'Alice in Dissonance',
+	'ASTRO PORT': 'Astro Port',
+	'Asylum entertainment': 'Asylum Entertainment',
+	'ATLUS': 'Atlus',
+	'CAPCOM': 'Capcom',
+	'CD PROJEKT RED': 'CD Projekt Red',
+	'DIMPS': 'Dimps',
+	'DONTNOD Entertainment': 'Dontnod Entertainment',
+	'FIVE-BN GAMES': 'Five-BN Games',
+	'GATO STUDIO': 'Gato Studio', #Also seen as "Gato Salvaje", although that doesn't transate exactly to the English name
+	'IDEA FACTORY ': 'Idea Factory', #Who puts a damn space there
+	'KITATUS STUDIOS': 'Kitatus Studios',
+	'OVERDRIVE': 'Overdrive',
+	'QUICKTEQUILA': 'Quicktequila',
+	'SEGA': 'Sega',
+	'SQUARE ENIX': 'Square Enix',
+	'XSEED': 'XSeed',
 
 	#These could be sorta like different brands of the same company, but I'm gonna go with the opinion that they should be treated as the same company. But this section could be like... subjective I guess
 	'Idea Factory International': 'Idea Factory',
