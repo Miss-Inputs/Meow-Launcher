6c9089052e55e79b4ad4b76dc98966f7690c5d16
Megan Leet <megan.leet@iinet.net.au>
2019-05-13 00:34:53 +1000

    Amiga: Set more info from filename tags.

--- platform_metadata/minor_systems.py
+++ platform_metadata/minor_systems.py
@@ -392,13 +392,60 @@ def add_amiga_info(game):
 		elif usage == 'Requires AGA':
 			chipset = 'AGA'
 		else:
-			#This would be stuff among the lines of "Requires "blah" to work
+			#The remainder is something like "Requires <some other software> to work", because it's a level editor or save editor or something like that
 			game.metadata.notes = usage
 
+		#info name="additional":
+		#Features Kid Gloves demo
+		#Features StarRay demo
+		#Features XR35 and KGP demos
+		#Mastered with virus
+		#info name="alt_disk": Names of some disks?
+		#info name="magazine": What magazine it came from?
+
+	for tag in game.filename_tags:
+		if tag == '[HD]':
+			game.metadata.specific_info['Requires-Hard-Disk'] = True
+			continue
+		if tag == '[WB]':
+			game.metadata.specific_info['Requires-Workbench'] = True
+			continue
+		
+		models = {'A1000', 'A1200', 'A4000', 'A2000', 'A3000', 'A2024', 'A2500', 'A4000T', 'A500', 'A500+', 'A570', 'A600', 'A600HD'}
+		for model in models:
+			if tag == ('(%s)' % model):
+				game.metadata.specific_info['Machine'] = model
+
+		#This should set machine to an array or something-separated list, tbh
+		if tag == '(A1200-A4000)':
+			game.metadata.specific_info['Machine'] = 'A4000'
+		elif tag == '(A2000-A3000)':
+			game.metadata.specific_info['Machine'] = 'A3000'
+		elif tag == '(A2500-A3000UX)':
+			game.metadata.specific_info['Machine'] = 'A3000UX'
+		elif tag == '(A500-A1000-A2000)':
+			game.metadata.specific_info['Machine'] = 'A2000'
+		elif tag == '(A500-A1000-A2000-CDTV)':
+			game.metadata.specific_info['Machine'] = 'A2000'
+		elif tag == '(A500-A1200)':
+			game.metadata.specific_info['Machine'] = 'A1200'
+		elif tag == '(A500-A2000)':
+			game.metadata.specific_info['Machine'] = 'A2000'
+		elif tag == '(A500-A600-A2000)':
+			game.metadata.specific_info['Machine'] = 'A2000'
+		elif tag == '(A500-A1200-A2000-A4000)':
+			game.metadata.specific_info['Machine'] = 'A4000'
+			
 	if not chipset:
 		for tag in game.filename_tags:
-			if tag in ('(AGA)', '(OCS-AGA)'):
+			if tag in ('(AGA)', '(OCS-AGA)', '(ECS-AGA)', '(AGA-CD32)'):
 				chipset = 'AGA'
+				break
+			if tag in ('(ECS)', '(ECS-OCS)'):
+				chipset = 'ECS'
+				break
+			if tag == 'OCS':
+				chipset = 'OCS'
 				break
 	game.metadata.specific_info['Chipset'] = chipset
 
