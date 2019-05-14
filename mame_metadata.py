89bca7f2c51862461f4412b65b17a9e74cd05fe7
Megan Leet <megan.leet@iinet.net.au>
2019-05-14 12:53:28 +1000

    Set that there, too

--- mame_metadata.py
+++ mame_metadata.py
@@ -512,13 +512,14 @@ def add_metadata_from_catlist(machine):
 	if genre == 'Electromechanical' and subgenre == 'Pinball':
 		#There are a few things under Arcade: Electromechanical / Utilities that are also pinball stuff, although perhaps not all of them. It only becomes apparent due to them using the "genpin" sample set
 		machine.metadata.platform = 'Pinball'
-	if genre == 'Handheld' and subgenre in ('Electronic Game', 'Home Videogame Console'):
+	if genre == 'Handheld' and subgenre == 'Electronic Game':
 		#Note: "Handheld / Electronic Game" could also be a tabletop system which takes AC input and you would not be able to hold in your hands at all (see also: cpacman), but since catlist.ini doesn't take that into account, I don't really have a way of doing so either
+		machine.metadata.platform = 'Handheld'
+	if genre == 'Handheld' and subgenre == 'Home Videogame Console':
 		#Home Videogame Console seems to be used for stuff that would be normally excluded due to having software lists and hence being a platform for other software (e.g. GBA), or stuff that ends up there because it has no software list yet (e.g. Gizmondo, Sony PocketStation), but also some stuff like kcontra (Contra handheld) that should definitely be called a handheld, or various "plug & play" (except without the plug) stuff like BittBoy 300 in 1 or VG Pocket
 		#Anyway that's why I put that there
 		#Other genres of handheld: Pocket Device - Pad - PDA; Child Computer (e.g. Speak & Spell) but those seem more suited to Standalone System particularly the former
-		machine.metadata.platform = 'Handheld'
-	
+		machine.metadata.platform = 'Handheld' if is_plug_and_play(machine, subgenre) else 'Standalone System'
 	if genre in ('Electromechanical', 'Utilities', 'Medal Game'):
 		machine.metadata.categories = [genre]
 	elif genre == 'Misc.' and subgenre in ('Laserdisc Simulator', 'Print Club', 'Unknown', 'Redemption'):
