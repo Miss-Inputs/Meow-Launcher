51b4531f4eede5d9463cbda17aaf97b85b97c3f5
Megan Leet <megan.leet@iinet.net.au>
2019-05-22 02:22:02 +1000

    Update series_detect_overrides.

--- data/series_detect_overrides.py
+++ data/series_detect_overrides.py
@@ -1,5 +1,7 @@
 series_overrides = {
 	#These names are too clever for my code to work properly, so I'll do them manually
+	'Fantastic 4': ('Fantastic 4', '1'),
+	'Fantastic 4 - Flame On': ('Fantastic 4', 'Flame On'), #Not sure what this is numerically
 	'Killer 7': (None, None),
 	'Mega Man X': ('Mega Man X', '1'),
 	'Metal Slug X': ('Metal Slug', '2X'),
