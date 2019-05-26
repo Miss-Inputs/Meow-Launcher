6a41fa5e75c5d34700df114b1e17513ce36c9108
Megan Leet <megan.leet@iinet.net.au>
2019-05-26 14:53:27 +1000

    Solve that debacle once and for all

--- platform_metadata/snes.py
+++ platform_metadata/snes.py
@@ -350,4 +350,13 @@ def add_snes_metadata(game):
 		if game.metadata.save_type == SaveType.Unknown and game.metadata.platform != 'Satellaview':
 			game.metadata.save_type = SaveType.Cart if software.has_data_area('nvram') else SaveType.Nothing
 		#We can actually get lorom/hirom from feature = slot. Hmm...
+
+		#Meh...
+		if software.name in ('ffant2', 'ffant2a'):
+			game.metadata.series = 'Final Fantasy'
+			game.metadata.series_index = '4'
+		elif software.name in ('ffant3', 'ffant3a', 'ffant3p'):
+			game.metadata.series = 'Final Fantasy'
+			game.metadata.series_index = '6'
+		
 	#Can't get input_info at this point as there's nothing to distinguish stuff that uses mouse/gun/etc
