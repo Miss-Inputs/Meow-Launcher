c8d82f78d3d316146133022d4cae758ff3032b92
Megan Leet <megan.leet@iinet.net.au>
2019-06-17 02:13:40 +1000

    Update mame_manufacturers.

--- data/mame_manufacturers.py
+++ data/mame_manufacturers.py
@@ -15,16 +15,21 @@ manufacturer_overrides = {
 	#Empire Entertainment == Empire Interactive?
 	#New Image Technologies == New Image?
 	#Naxat == Naxat Soft?
+	#RCM Group == RCM?
+
 	#The SNES game Super Godzilla (USA) has a publisher of literally "Super Godzilla". Wait what? That can't be right. Should be Toho right? Same with Tetris (Japan) for Megadrive. Unless they meant The Tetris Company there. But then I dunno
 	#Leave Atari Games > Atari and Midway Games > Midway alone, because if I try to comperehend the timeline of which is what and who owned the rights to which brand name and who owned who at any given time, I would die of confusion
 	#Marvelous Entertainment and Marvelous Interactive also are different (due to mergers) and I gotta remember that
 	
 	'20th Century Fox Video Games': '20th Century Fox',
+	'3DO Company': '3DO',
 	'Absolute': 'Absolute Entertainment', #Hmm, not sure if it'd be better to do this the other way around
 	'Acclaim Entertainment': 'Acclaim',
 	'American Softworks Company': 'American Softworks',
 	'APF Electronics': 'APF',
 	'ASCII Entertainment': 'ASCII',
+	'Bally Midway Mfg.': 'Bally Midway',
+	'Bally Midway MFG.': 'Bally Midway',
 	'Bit Corp': 'Bit Corporation',
 	'BPS': 'Bullet-Proof Software', #I hope nobody else uses that acronym
 	'Broderbund': 'Brøderbund',
@@ -41,7 +46,9 @@ manufacturer_overrides = {
 	'DataSoft': 'Datasoft',
 	'Dempa Shinbunsha': 'Dempa',
 	'Digital Equipment Corporation': 'DEC',
+	'Ectron Eletrônica Ltda.': 'Ectron Eletrônica',
 	'Eidos Interactive': 'Eidos',
+	'Electronics Arts': 'Electronic Arts',
 	'Elite': 'Elite Systems',
 	'Entex Industries': 'Entex',
 	'First Star': 'First Star Software',
@@ -70,6 +77,7 @@ manufacturer_overrides = {
 	'Microprose Games': 'MicroProse',
 	'NEC Home Electronics': 'NEC',
 	'Nihon Telenet': 'Telenet', #I guess
+	'Nippon System': 'Nihon System',
 	'Ocean Software': 'Ocean',
 	'Ocean Software Limited': 'Ocean',
 	'Omage Micott': 'Omega Micott', #I have a feeling I'm the one who's wrong here. Never did quality check the Wonderswan licensees
@@ -95,8 +103,11 @@ manufacturer_overrides = {
 	'Taito America Corporation': 'Taito America',
 	'Team 17': 'Team17',
 	'TecMagik Entertainment': 'TecMagik',
+	'Techno Soft': 'Technosoft',
 	'TecToy': 'Tec Toy',
+	'Telenet Japan': 'Telenet',
 	'T*HQ': 'THQ', #Why.
+	'Tiger Electronics': 'Tiger', #Or should this be other way around
 	'Titus Software': 'Titus',
 	'UA Limited': 'UA',
 	'V.Fame': 'Vast Fame',
@@ -112,7 +123,7 @@ manufacturer_overrides = {
 	#This isn't a case of a company formatting its name in different ways, but it's where a company's renamed itself, and maybe I shouldn't do this...
 	'Alpha Denshi': 'ADK', #Renamed in 1993
 	'Ubi Soft': 'Ubisoft', #I hate that they used to spell their name with a space so this is valid. But then, don't we all hate Ubisoft for one reason or another?
-	#Video Technology > VTech
+	'Video Technology': 'VTech',
 
 	#Brand names that are definitely the same company but insist on using some other name... maybe I shouldn't change these either, but then, I'm going to
 	'Atarisoft': 'Atari', #Atarisoft is just a brand name and not an actual company, so I guess I'll do this
@@ -189,17 +200,20 @@ manufacturer_overrides = {
 
 	#Maybe typos?
 	'ChinSoft': 'Chunsoft', #gbcolor/furaish2
-	'Commonweaalth': 'Commonwealth',
-	'Connonwealth': 'Commonwealth',
 	'Elite System': 'Elite Systems',
 	'Hi-Tech Expression': 'Hi-Tech Expressions',
 	'Hi Tech Expressions': 'Hi-Tech Expressions',
 	'Jungle\'s Soft': 'Jungle Soft',
-	'Mindscapce': 'Mindscape', #Yeah okay, that _definitely_ is a typo
 	'Pack-In-Video': 'Pack-In Video',
-	'Sydney Developmeent ': 'Sydney Development',
 	'Take Two Interactive': 'Take-Two Interactive',
 	'Watera': 'Watara',
+
+	#Definitely typos
+	'Activison': 'Activision',
+	'Commonweaalth': 'Commonwealth',
+	'Connonwealth': 'Commonwealth',
+	'Mindscapce': 'Mindscape',
+	'Sydney Developmeent ': 'Sydney Development',
 
 	'unknown': '<unknown>', #This shows up in sv8000 software list, so it might actually just be Bandai, but when you presume you make a pres out of u and me, so we'll just lump it in with the other unknowns
 }
