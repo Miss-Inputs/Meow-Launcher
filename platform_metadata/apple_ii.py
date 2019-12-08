f1a893bc929e7115726b3a95c14312061b97ef25
Megan Leet <megan.leet@iinet.net.au>
2019-12-09 00:43:45 +1100

    xvxvzcz

--- platform_metadata/apple_ii.py
+++ platform_metadata/apple_ii.py
@@ -67,9 +67,10 @@ def parse_woz_meta_chunk(game, chunk_data):
 		except ValueError: #Oh I guess this includes UnicodeDecodeError
 			continue
 
-		if key in ('side', 'side_name', 'contributor', 'image_date', 'collection'):
+		if key in ('side', 'side_name', 'contributor', 'image_date', 'collection', 'requires_platform'):
 			#No use for these
 			#"collection" is not part of the spec but it shows up and it just says where the image came from
+			#requires_platform is not either, it just seems to be "apple2" so far and I don't get it
 			pass
 		elif key == 'version':
 			#Note that this is free text
