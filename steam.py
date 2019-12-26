b375380fa5f7fa5ee5b94ff082d6e4d4239f65d5
Megan Leet <megan.leet@iinet.net.au>
2019-12-26 16:11:53 +1100

    If we're gonna hardcode a path at least make it consistent

--- steam.py
+++ steam.py
@@ -214,7 +214,7 @@ def look_for_icon(icon_hash):
 
 				#Get the biggest image file and assume that's the best icon we can have
 				extracted_icon_file = sorted(icon_files, key=lambda zip_info: zip_info.file_size, reverse=True)[0]
-				extracted_icon_folder = os.path.join(main_config.image_folder, 'icons', 'extracted_from_zip', icon_hash)
+				extracted_icon_folder = os.path.join(main_config.image_folder, 'Icon', 'extracted_from_zip', icon_hash)
 				return zip_file.extract(extracted_icon_file, path=extracted_icon_folder)
 
 	raise IconNotFoundError('{0} not found'.format(icon_hash))
