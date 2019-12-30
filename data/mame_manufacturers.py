42e5a8c267f177dfa66e79f4b5770e53afe6dc39
Megan Leet <megan.leet@iinet.net.au>
2019-12-31 01:23:20 +1100

    Update mame_manufacturers and sort it for no particular reason

--- data/mame_manufacturers.py
+++ data/mame_manufacturers.py
@@ -5,7 +5,6 @@ manufacturer_overrides = {
 	#Yeah let's make this a big TODO to verify what formatting companies actually use themselves
 
 	#TODO: Are ATW > ATW USA the same or a regional branch?
-	#Should NEC Avenue just be called NEC?
 	#Toshiba EMI > Toshiba? Or is that a combination of the things
 	#Are CBS Electronics and CBS Software the same? Seems like they're both owned by CBS the American TV company, the former is for various Atari 2600/5200/7800 games they published and distributing the ColecoVision outside USA; and the latter is basically licensed Sesame Street games?
 	#Are Fox Interactive, Fox Video Games, 20th Century Fox all the same?
@@ -21,107 +20,126 @@ manufacturer_overrides = {
 	#Leave Atari Games > Atari and Midway Games > Midway alone, because if I try to comperehend the timeline of which is what and who owned the rights to which brand name and who owned who at any given time, I would die of confusion
 	#Marvelous Entertainment and Marvelous Interactive also are different (due to mergers) and I gotta remember that
 	
-	'20th Century Fox Video Games': '20th Century Fox',
-	'3DO Company': '3DO',
 	'Absolute': 'Absolute Entertainment', #Hmm, not sure if it'd be better to do this the other way around
-	'Acclaim Entertainment': 'Acclaim',
-	'American Softworks Company': 'American Softworks',
-	'APF Electronics': 'APF',
-	'ASCII Entertainment': 'ASCII',
-	'Bally Midway Mfg.': 'Bally Midway',
-	'Bally Midway MFG.': 'Bally Midway',
+	'Big Ben Interactive': 'BigBen Interactive',
 	'Bit Corp': 'Bit Corporation',
 	'BPS': 'Bullet-Proof Software', #I hope nobody else uses that acronym
 	'Broderbund': 'Brøderbund',
-	'Broderbund Software': 'Brøderbund',
-	'Brøderbund Software': 'Brøderbund',
-	'Broderbund software': 'Brøderbund',
-	'California Pacific Computer': 'California Pacific',
-	'Coconuts Japan Entertainment': 'Coconuts Japan',
-	'Commodore Business Machines': 'Commodore',
 	'Creative Electronics And Software': 'Creative Electronics & Software',
-	'Creative Software': 'Creative',
 	'Cryo': 'Cryo Interactive',
 	'D3 Publishing': 'D3 Publisher',
-	'Data East Corporation': 'Data East',
 	'DataSoft': 'Datasoft',
-	'Dempa Shinbunsha': 'Dempa',
-	'Digital Equipment Corporation': 'DEC',
-	'Ectron Eletrônica Ltda.': 'Ectron Eletrônica',
-	'Eidos Interactive': 'Eidos',
 	'Electronics Arts': 'Electronic Arts',
 	'Elite': 'Elite Systems',
-	'Entex Industries': 'Entex',
 	'First Star': 'First Star Software',
-	'Funtech Entertainment': 'Funtech',
-	'General Consumer Electronics': 'GCE',
-	'Gradiente Entertainment Ltda.': 'Gradiente Entertainment',
-	'Grandslam Entertainments': 'Grandslam',
 	'Gremlin Interactive': 'Gremlin Graphics',
-	'HAL Kenkyuujo': 'HAL', #Literally "HAL Laboratory"
-	'HAL Laboratory': 'HAL',
-	'Hasbro Interactive': 'Hasbro',
-	'Hewlett-Packard': 'HP',
 	'HiCom': 'Hi-Com',
 	'Hudson': 'Hudson Soft',
-	'Human Entertainment': 'Human',
-	'International Business Machines': 'IBM',
-	'JoWooD Entertainment AG': 'JoWooD Entertainment',
-	'Kaneko Elc.': 'Kaneko',
 	'Kyugo': 'Kyugo Boueki',
-	'K-Tel Vision': 'K-Tel',
 	'Laser Beam': 'Laser Beam Entertainment',
-	'Loriciel Software': 'Loriciel',
 	'Malibu Games': 'Malibu Interactive',
-	'Merit Industries': 'Merit',
 	'MicroCabin': 'Micro Cabin', #Annoying alternate spelling because they officially use both just to be annoying
 	'Microdigital': 'Micro Digital',
 	'Microlab': 'Micro Lab',
-	'Microprose Games': 'MicroProse',
-	'NEC Home Electronics': 'NEC',
 	'Nihon Telenet': 'Telenet', #I guess
 	'Nippon System': 'Nihon System',
-	'Ocean Software': 'Ocean',
-	'Ocean Software Limited': 'Ocean',
 	'Omage Micott': 'Omega Micott', #I have a feeling I'm the one who's wrong here. Never did quality check the Wonderswan licensees
-	'Omori Electric': 'Omori',
 	'Palm Inc': 'Palm',
-	'Petaco S.A.': 'Petaco',
-	'Playmates Interactive': 'Playmates',
 	'PonyCa': 'Pony Canyon',
 	'ProSoft': 'Prosoft',
-	'Rare Coin-It': 'Rare',
 	'Sales Curve': 'The Sales Curve',
-	'Sales Curve Interactive': 'The Sales Curve',
-	'Sammy Entertainment': 'Sammy',
-	'Sega Enterprises': 'Sega',
-	'Seta Corporation': 'Seta',
-	'Sierra Entertainment': 'Sierra',
-	'Sierra On-Line': 'Sierra',
-	'Sigma Enterprises': 'Sigma', #Every time I see this line I keep thinking "sigma balls", just thought you should know
 	'Software Toolworks': 'The Software Toolworks', #It doesn't seem right that the "correct" one is the latter, but it's used more often, so I guess it is
-	'Spinnaker Software': 'Spinnaker',
-	'Spinnaker Software ': 'Spinnaker', #bah should I start doing .rstrip() before the thing
-	'Sunrise Software': 'Sunrise',
-	'Swing! Entertainment Media': 'Swing! Entertainment',
-	'System 3 Software': 'System 3',
+	'Team 17': 'Team17',
+	'Techno Soft': 'Technosoft',
+	'TecToy': 'Tec Toy',
+	'T*HQ': 'THQ', #Why.
+	'V.Fame': 'Vast Fame',
+	'Walt Disney': 'Disney',
+
+	#Suffixes where the suffix is often not used
+	#"Company"
+	'3DO Company': '3DO',
+	'American Softworks Company': 'American Softworks',
+	#"Corporation"
+	'Visco Corporation': 'Visco',
 	'Taito Corporation': 'Taito',
 	'Taito Corporation Japan': 'Taito',
 	'Taito America Corporation': 'Taito America',
-	'Team 17': 'Team17',
-	'TecMagik Entertainment': 'TecMagik',
-	'Techno Soft': 'Technosoft',
-	'TecToy': 'Tec Toy',
-	'Telenet Japan': 'Telenet',
-	'T*HQ': 'THQ', #Why.
+	'Data East Corporation': 'Data East',
+	'Seta Corporation': 'Seta',
+	#"Electric"
+	'Kaneko Elc.': 'Kaneko',
+	'Omori Electric': 'Omori',
+	#"Electronics"
+	'APF Electronics': 'APF',
 	'Tiger Electronics': 'Tiger', #Or should this be other way around
-	'Titus Software': 'Titus',
-	'UA Limited': 'UA',
-	'V.Fame': 'Vast Fame',
-	'Visco Corporation': 'Visco',
-	'Walt Disney': 'Disney',
-	'Warner Bros. Games': 'Warner Bros',
+	#"Enterprises"
+	'Sega Enterprises': 'Sega',
+	'Sigma Enterprises': 'Sigma', #Every time I see this line I keep thinking "sigma balls", just thought you should know
+	#"Entertainment"
+	'Acclaim Entertainment': 'Acclaim',
+	'ASCII Entertainment': 'ASCII',
 	'Williams Entertainment': 'Williams',
+	'Coconuts Japan Entertainment': 'Coconuts Japan',
+	'Sierra Entertainment': 'Sierra',
+	'Sammy Entertainment': 'Sammy',
+	'Funtech Entertainment': 'Funtech',
+	'Human Entertainment': 'Human',
+	'Grandslam Entertainments': 'Grandslam',
+	'TecMagik Entertainment': 'TecMagik',
+	#"Games"
+	'Warner Bros. Games': 'Warner Bros',
+	'Microprose Games': 'MicroProse',
+	#"Industries"
+	'Entex Industries': 'Entex',
+	'Merit Industries': 'Merit',
+	#"Interactive"
+	'Playmates Interactive': 'Playmates',
+	'Sales Curve Interactive': 'The Sales Curve',
+	'Hasbro Interactive': 'Hasbro',
+	'Eidos Interactive': 'Eidos',
+	#"Limited"
+	'Ectron Eletrônica Ltda.': 'Ectron Eletrônica',
+	'UA Limited': 'UA',
+	'Gradiente Entertainment Ltda.': 'Gradiente Entertainment',
+	#"Manufacturing"
+	'Bally Midway Mfg.': 'Bally Midway',
+	'Bally Midway MFG.': 'Bally Midway',
+	#"Software"
+	'Broderbund Software': 'Brøderbund',
+	'Brøderbund Software': 'Brøderbund',
+	'Broderbund software': 'Brøderbund',
+	'Creative Software': 'Creative',
+	'Ocean Software': 'Ocean',
+	'Ocean Software Limited': 'Ocean',
+	'Titus Software': 'Titus',
+	'Spinnaker Software': 'Spinnaker',
+	'Spinnaker Software ': 'Spinnaker', #bah should I start doing .rstrip() before the thing
+	'System 3 Software': 'System 3',
+	'Loriciel Software': 'Loriciel',
+	'Sunrise Software': 'Sunrise',
+	#"Studios"
+	'Angel Studios': 'Angel',
+	#"Video Games"
+	'20th Century Fox Video Games': '20th Century Fox',	
+	#Other junk
+	'Sierra On-Line': 'Sierra',
+	'Commodore Business Machines': 'Commodore',
+	'NEC Home Electronics': 'NEC',
+	'California Pacific Computer': 'California Pacific',
+	'HAL Kenkyuujo': 'HAL', #Literally "HAL Laboratory"
+	'HAL Laboratory': 'HAL',
+	'Dempa Shinbunsha': 'Dempa',
+	'JoWooD Entertainment AG': 'JoWooD Entertainment',
+	'K-Tel Vision': 'K-Tel',
+	'Petaco S.A.': 'Petaco',
+	'Swing! Entertainment Media': 'Swing! Entertainment',
+
+	#Acronyms that nobody actually expands
+	'Digital Equipment Corporation': 'DEC',
+	'International Business Machines': 'IBM',
+	'General Consumer Electronics': 'GCE',
+	'Hewlett-Packard': 'HP',
 
 	#Sometimes companies go by two different names and like... maybe I should leave those alone, bleh I hate decision making
 	'DSI Games': 'Destination Software',
@@ -132,6 +150,7 @@ manufacturer_overrides = {
 	'Alpha Denshi': 'ADK', #Renamed in 1993
 	'Ubi Soft': 'Ubisoft', #I hate that they used to spell their name with a space so this is valid. But then, don't we all hate Ubisoft for one reason or another?
 	'Video Technology': 'VTech',
+	'Rare Coin-It': 'Rare',
 
 	#Brand names that are definitely the same company but insist on using some other name... maybe I shouldn't change these either, but then, I'm going to
 	'Atarisoft': 'Atari', #Atarisoft is just a brand name and not an actual company, so I guess I'll do this
@@ -146,6 +165,7 @@ manufacturer_overrides = {
 	'Mattel Electronics': 'Mattel',
 	'Mattel Interactive': 'Mattel',
 	'Mattel Media': 'Mattel',
+	'NEC Avenue': 'NEC',
 	'Nihon Bussan': 'Nichibutsu',
 	'Nihonbussan': 'Nichibutsu', #In the event that we figure out we shouldn't change the above, we should at least consistentify this formatting
 	'Sony Computer Entertainment': 'Sony',
@@ -187,6 +207,7 @@ manufacturer_overrides = {
 	'ポニカ (PonyCa)': 'Pony Canyon',
 	'マイクロネット (Micronet)': 'Micronet',
 	'マカダミアソフト (Macadamia Soft)': 'Macadamia Soft',
+	'富士通 (Fujitsu)': 'Fujitsu',
 	'工画堂スタジオ (Kogado Studio)': 'Kogado Studio',
 	'日本ソフトバンク (Nihon SoftBank)': 'Nihon SoftBank',
 	'日本テレネット (Nihon Telenet)': 'Telenet',
@@ -223,12 +244,13 @@ manufacturer_overrides = {
 	'Commonweaalth': 'Commonwealth',
 	'Connonwealth': 'Commonwealth',
 	'Mindscapce': 'Mindscape',
+	'Minscape': 'Mindscape',
 	'Sydney Developmeent ': 'Sydney Development',
 	'unknown': '<unknown>', #This shows up in sv8000 software list, so it might actually just be Bandai, but when you presume you make a pres out of u and me, so we'll just lump it in with the other unknowns
 }
 
 dont_remove_suffix = [
-	#Normally we run junk_suffixes on stuff to remove "Corporation" "Co." at the end but sometimes we shouldn't
+	#Normally we run junk_suffixes on stuff to remove "Corp" "Co." at the end but sometimes we shouldn't
 	'Bit Corp', #We will then fix that up later
 	'Zonov and Co.',
 ]
