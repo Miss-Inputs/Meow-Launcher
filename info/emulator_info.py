0f0c4a5f272bc920447b47a2c9b74cdaf7271a93
Megan Leet <megan.leet@iinet.net.au>
2020-06-04 16:33:52 +1000

    Add todo note to enable my procrastination habit

--- info/emulator_info.py
+++ info/emulator_info.py
@@ -55,6 +55,7 @@ emulators = {
 	#Or is it good? Have not tried it in a fair bit
 	'Dolphin': EmulatorInfo(EmulatorStatus.Good, command_lines.dolphin, ['iso', 'ciso', 'gcm', 'gcz', 'tgc', 'elf', 'dol', 'wad', 'wbfs', 'm3u'], []),
 	'DuckStation': EmulatorInfo(EmulatorStatus.Good, LaunchParams('duckstation-qt', ['-batch', '-fullscreen', '$<path>']), ['bin', 'img', 'cue', 'chd', 'exe'], []),
+	#TODO: The compatibility.xml file is there for the reading, but due to how the installation works it's not in a specific location. Do something with that when I get around to doing that thing I needed to do
 	'Flycast': EmulatorInfo(EmulatorStatus.Good, command_lines.flycast, ['gdi', 'cdi', 'chd', 'cue'], []),
 	'FS-UAE': EmulatorInfo(EmulatorStatus.Good, command_lines.fs_uae, ['iso', 'cue', 'adf', 'ipf'], []),
 	#Note that .ipf files need a separately downloadable plugin. We could detect the presence of that, I guess
