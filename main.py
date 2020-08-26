698ef45b1708641b168e97e4232b661cb9eceacd
Megan Leet <megan.leet@iinet.net.au>
2020-08-26 19:35:32 +1000

    Whoops

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-import config
+import config.main_config
 import dos
 import mac
 import mame_machines
@@ -12,6 +12,8 @@ import series_detect
 import disambiguate
 import organize_folders
 
+main_config = config.main_config.main_config
+
 def main(progress_function, mame_enabled=True, roms_enabled=True, dos_enabled=True, mac_enabled=True, scummvm_enabled=True, steam_enabled=True):
 	def call_progress_function(data, should_increment=True):
 		if progress_function:
@@ -19,11 +21,11 @@ def main(progress_function, mame_enabled=True, roms_enabled=True, dos_enabled=Tr
 
 	call_progress_function('Creating output folder')
 
-	if config.main_config.full_rescan:
-		if os.path.isdir(config.main_config.output_folder):
-			for f in os.listdir(config.main_config.output_folder):
-				os.unlink(os.path.join(config.main_config.output_folder, f))
-	os.makedirs(config.main_config.output_folder, exist_ok=True)
+	if main_config.full_rescan:
+		if os.path.isdir(main_config.output_folder):
+			for f in os.listdir(main_config.output_folder):
+				os.unlink(os.path.join(main_config.output_folder, f))
+	os.makedirs(main_config.output_folder, exist_ok=True)
 
 	if mame_enabled:
 		call_progress_function('Scanning MAME machines')
@@ -44,13 +46,13 @@ def main(progress_function, mame_enabled=True, roms_enabled=True, dos_enabled=Tr
 		call_progress_function('Scanning Steam games')
 		steam.process_steam()
 
-	if not config.main_config.full_rescan:
+	if not main_config.full_rescan:
 		call_progress_function('Removing games which no longer exist')
 		remove_nonexistent_games.remove_nonexistent_games()
 	else:
 		call_progress_function(None)
 
-	if config.main_config.get_series_from_name:
+	if main_config.get_series_from_name:
 		call_progress_function('Detecting series')
 		series_detect.detect_series_for_all_desktops()
 	else:
@@ -59,7 +61,7 @@ def main(progress_function, mame_enabled=True, roms_enabled=True, dos_enabled=Tr
 	call_progress_function('Disambiguating names')
 	disambiguate.disambiguate_names()
 
-	if config.main_config.organize_folders:
+	if main_config.organize_folders:
 		call_progress_function('Organizing into folders')
 		organize_folders.move_into_folders()
 	else:
