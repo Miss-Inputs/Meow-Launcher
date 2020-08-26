79d8459589cafecf2a12939e89e991837e5c96f3
Megan Leet <megan.leet@iinet.net.au>
2020-08-26 19:37:46 +1000

    Whoops I don't need that garbage

--- config/__init__.py
+++
@@ -1,7 +0,0 @@
-# from .main_config_loader import Config
-# from .system_config import SystemConfigs
-# from .emulator_config import EmulatorConfigs
-
-# main_config = Config.getConfig()
-# system_configs = SystemConfigs.getConfigs().configs
-# emulator_configs = EmulatorConfigs.getConfigs().configs
