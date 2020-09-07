b7bf1cf2ad22a31f9286b94c6efb9f4dd561b9ab
Megan Leet <megan.leet@iinet.net.au>
2020-09-07 20:26:59 +1000

    blah

--- pc_common_metadata.py
+++ pc_common_metadata.py
@@ -31,22 +31,39 @@ def look_for_icon_in_folder(folder, look_for_any_ico=True):
 				return os.path.join(folder, f)
 	return None
 
+def try_detect_unity(folder):
+	for f in os.listdir(folder):
+		if f.endswith('_Data'):
+			#This folder will probably be named after an executable in the folder? Maybe
+			#appinfo.txt contains the publisher on line 1, and the name (which sometimes is formatted weirdly) on line 2
+			if os.path.isfile(os.path.join(folder, f, 'Managed', 'UnityEngine.dll')):
+				return True
+	return False
+
+
 def try_and_detect_engine_from_folder(folder):
 	dir_entries = list(os.scandir(folder))
 	files = [f.name.lower() for f in dir_entries if f.is_file()]
 	subdirs = [f.name.lower() for f in dir_entries if f.is_dir()]
 
-	if 'renpy' in subdirs:
+	#These are simple enough to detect with just one line…
+	if 'renpy' in subdirs and 'game' in subdirs and 'lib' in subdirs:
 		return "Ren'Py"
-	if 'data.dcp' in files:
+	if 'data.dcp' in files or 'data_sd.dcp' in files or 'data_hd.dcp' in files:
+		#Hmm, hopefully there's no false positives here without actually looking inside the file
 		return 'Wintermute'
 	if 'acsetup.cfg' in files:
+		#The better way to do this would be to look for 'Adventure Creator Game File' in dat or exe I guess (and then we can get game title from there), but also the effort way
 		return 'Adventure Game Studio'
 	if 'rpg_rt.exe' in files:
+		#TODO: Get title from RPG_RT.ini section RPG_RT line GameTitle= (this is not always there though…)
 		return 'RPG Maker 2000/2003'
+	if 'logdir' in files and 'object' in files and 'picdir' in files and 'viewdir' in files and 'snddir' in files and 'vol.0' in files and 'words.tok' in files:
+		#Apparently there can be .wag files?
+		return 'AGI' #v2
+
 	if os.path.basename(folder).lower() + '.uproject' in files:
 		return 'Unreal Engine 4'
-	
 	if any(f.endswith('.rgssad') for f in files):
 		return 'RPG Maker XP/VX'
 	if any(f.endswith('.cf') for f in files):
@@ -55,6 +72,7 @@ def try_and_detect_engine_from_folder(folder):
 
 	#Hmm should I be refactoring these lines down here
 	if os.path.isfile(os.path.join(folder, 'assets', 'game.unx')):
+		#game.win on Windows… that's too false-positivey for me
 		return 'GameMaker'
 	if 'adobe air' in subdirs or os.path.isdir(os.path.join(folder, 'runtimes', 'Adobe AIR')):
 		return 'Adobe AIR'
@@ -63,15 +81,13 @@ def try_and_detect_engine_from_folder(folder):
 		return 'Adobe AIR'
 	if 'bin' in subdirs and 'platform' in subdirs:
 		for f in os.listdir(folder):
-			if os.path.isdir(os.path.join(folder, f)):
+			if os.path.isdir(os.path.join(folder, f)): #'hl2', 'ep1', etc
 				if os.path.isfile(os.path.join(folder, f, 'gameinfo.txt')):
+					#gameinfo.txt contains metadata but then this would probably only appear on games that are from Steam and we get all the metadata from there anyway
 					return 'Source'
+	if try_detect_unity(folder):
+		return 'Unity'
 	for f in os.listdir(folder):
-		#Sometimes UnityPlayer.dll is not always there I think
-		if f.endswith('_Data'):
-			#appinfo.txt contains the publisher on line 1, and the name (which sometimes is formatted weirdly) on line 2
-			if os.path.isfile(os.path.join(folder, f, 'Managed', 'UnityEngine.dll')):
-				return 'Unity'
 		if (f != 'Game' and f.endswith('Game')) or f == 'P13':
 			if os.path.isdir(os.path.join(folder, f)):
 				if os.path.isfile(os.path.join(folder, f, 'CookedPC', 'Engine.u')):
