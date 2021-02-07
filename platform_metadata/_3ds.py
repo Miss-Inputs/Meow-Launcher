afb89c89fb46d2d7c9d313942bf25674980d5dd5
Megan Leet <megan.leet@iinet.net.au>
2021-02-07 19:42:37 +1100

    Should use IntFlag here

--- platform_metadata/_3ds.py
+++ platform_metadata/_3ds.py
@@ -5,7 +5,7 @@ except ModuleNotFoundError:
 	have_pillow = False
 
 import os
-from enum import Enum, Flag
+from enum import Enum, IntFlag
 from xml.etree import ElementTree
 
 import input_metadata
@@ -22,7 +22,7 @@ from .gametdb import TDB, add_info_from_tdb
 from .wii import parse_ratings
 
 
-class _3DSRegionCode(Flag):
+class _3DSRegionCode(IntFlag):
 	Japan = 1
 	USA = 2
 	Europe = 4
