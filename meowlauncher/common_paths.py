493008520e1268c1547b89f48f449888b2f9c366
Megan Leet <megan.leet@iinet.net.au>
2021-11-19 04:40:23 +1100

    Naming consistency, let's settle on putting a space here

--- meowlauncher/common_paths.py
+++ meowlauncher/common_paths.py
@@ -2,6 +2,6 @@ from pathlib import Path
 
 
 #TODO: Get this in a less hardcody cross-platform way, I guess
-config_dir = Path('~/.config/MeowLauncher').expanduser()
-data_dir = Path('~/.local/share/MeowLauncher').expanduser()
-cache_dir = Path('~/.cache/MeowLauncher').expanduser()
+config_dir = Path('~/.config/Meow Launcher').expanduser()
+data_dir = Path('~/.local/share/Meow Launcher').expanduser()
+cache_dir = Path('~/.cache/Meow Launcher').expanduser()
