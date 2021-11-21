7f6725120a940a9b52345853024682ccdb79d1d4
Megan Leet <megan.leet@iinet.net.au>
2021-11-21 11:06:34 +1100

    Secret evil refactoring

--- meowlauncher/output/desktop_files.py
+++ meowlauncher/output/desktop_files.py
@@ -1,9 +1,8 @@
 import configparser
-import os
 import re
 from collections.abc import Collection
 from enum import Enum, Flag
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 try:
 	from PIL import Image
@@ -12,11 +11,11 @@ except ModuleNotFoundError:
 	have_pillow = False
 
 from meowlauncher.config.main_config import main_config
+from meowlauncher.games.mame_common.machine import Machine
+from meowlauncher.games.mame_common.software_list import Software, SoftwareList
 from meowlauncher.util.io_utils import ensure_exist, pick_new_filename
 from meowlauncher.util.utils import (clean_string, find_filename_tags_at_end,
                                      remove_filename_tags)
-from meowlauncher.games.mame_common.software_list import Software, SoftwareList
-from meowlauncher.games.mame_common.machine import Machine
 
 if TYPE_CHECKING:
 	from meowlauncher.launch_command import LaunchCommand
@@ -29,6 +28,44 @@ id_section_name = 'ID'
 junk_section_name = 'Junk'
 image_section_name = 'Images'
 
+def _write_field(desktop: configparser.ConfigParser, section_name: str, key_name: str, value: Any, image_base_path: str):
+	value_as_string: str
+	if have_pillow and isinstance(value, Image.Image):
+		this_image_folder = main_config.image_folder.joinpath(key_name)
+		this_image_folder.mkdir(exist_ok=True, parents=True)
+		image_path = this_image_folder.joinpath(image_base_path + '.png')
+		value.save(image_path, 'png')
+		value_as_string = str(image_path)
+
+	if isinstance(value, Collection) and not isinstance(value, str):
+		if not value:
+			return
+		value_as_string = ';'.join('None' if item is None else item.name if isinstance(item, Enum) else str(item) for item in value)
+	elif isinstance(value, Enum):
+		if value.name:
+			value_as_string = value.name
+		elif isinstance(value, Flag):
+			value_as_string = str(value).replace('|', ';')
+			value_as_string = value_as_string[value_as_string.find('.') + 1:]
+	elif isinstance(value, Software):
+		value_as_string = f'{value.name} ({value.description})'
+	elif isinstance(value, SoftwareList):
+		value_as_string = f'{value.name} ({value.description})'
+	elif isinstance(value, Machine):
+		value_as_string = f'{value.basename} ({value.name})'
+	else:
+		value_as_string = str(value)
+
+	cleaned_key_name = key_name.replace('_', '-').replace(' ', '-').replace('?', '').replace('/', '')
+	#value_as_string = clean_string(value_as_string.strip(), True)
+
+	section_writer = desktop[section_prefix + section_name]
+	if '\n' in value_as_string:
+		for i, line in enumerate(value_as_string.splitlines()):
+			section_writer[f'{cleaned_key_name}-Line-{i}'] = line
+	else:
+		section_writer[cleaned_key_name] = value_as_string
+
 def make_linux_desktop_for_launcher(launcher: 'Launcher'):
 	name = launcher.game.name
 
@@ -38,10 +75,10 @@ def make_linux_desktop_for_launcher(launcher: 'Launcher'):
 	if launcher.runner.is_emulated:
 		launcher.game.metadata.emulator_name = launcher.runner.name
 
+	#TODO: Merge with make_linux_desktop once we get rid of make_launcher
 	_make_linux_desktop(launcher.command, name, launcher.game.metadata, filename_tags, launcher.game_type, launcher.game_id)
 
 def _make_linux_desktop(launcher: 'LaunchCommand', display_name: str, metadata: 'Metadata', filename_tags: Collection[str], game_type, game_id):
-	#TODO: Merge with above once we get rid of make_launcher
 	filename = pick_new_filename(main_config.output_folder, display_name, 'desktop')
 	
 	path = main_config.output_folder.joinpath(filename)
@@ -75,50 +112,11 @@ def _make_linux_desktop(launcher: 'LaunchCommand', display_name: str, metadata: 
 		if not section:
 			continue
 		configwriter.add_section(section_prefix + section_name)
-		section_writer = configwriter[section_prefix + section_name]
 
 		for k, v in section.items():
 			if v is None:
 				continue
-
-			use_image_object = False
-			value_as_string: str
-			if have_pillow:
-				if isinstance(v, Image.Image):
-					use_image_object = True
-					this_image_folder = main_config.image_folder.joinpath(k)
-					this_image_folder.mkdir(exist_ok=True, parents=True)
-					image_path = this_image_folder.joinpath(filename + '.png')
-					v.save(image_path, 'png')
-					value_as_string = str(image_path)
-
-			if isinstance(v, Collection) and not isinstance(v, str):
-				if not v:
-					continue
-				value_as_string = ';'.join('None' if item is None else item.name if isinstance(item, Enum) else str(item) for item in v)
-			elif isinstance(v, Enum):
-				if v.name:
-					value_as_string = v.name
-				elif isinstance(v, Flag):
-					value_as_string = str(v).replace('|', ';')
-					value_as_string = value_as_string[value_as_string.find('.') + 1:]
-			elif isinstance(v, Software):
-				value_as_string = f'{v.name} ({v.description})'
-			elif isinstance(v, SoftwareList):
-				value_as_string = f'{v.name} ({v.description})'
-			elif isinstance(v, Machine):
-				value_as_string = f'{v.basename} ({v.name})'
-
-			elif not use_image_object:
-				value_as_string = str(v)
-
-			key_name = k.replace('_', '-').replace(' ', '-').replace('?', '').replace('/', '')
-			value_as_string = clean_string(value_as_string.strip(), True)
-			if '\n' in value_as_string:
-				for i, line in enumerate(value_as_string.splitlines()):
-					section_writer[f'{key_name}-Line-{i}'] = line
-			else:
-				section_writer[key_name] = value_as_string
+			_write_field(configwriter, section_name, k, v, filename)
 
 	if section_prefix + image_section_name in configwriter:
 		keys_to_try = ('Icon', ) + main_config.use_other_images_as_icons
@@ -128,11 +126,11 @@ def _make_linux_desktop(launcher: 'LaunchCommand', display_name: str, metadata: 
 				break
 
 	ensure_exist(path)
-	with open(path, 'wt', encoding='utf-8') as f:
+	with path.open('wt', encoding='utf-8') as f:
 		configwriter.write(f)
 
-	#Set executable, but also set everything else because whatever
-	os.chmod(path, 0o7777)
+	#Set executable, but also set everything else because whatever, partially because I can't remember what I would need to do to get the original mode and | it with executable
+	path.chmod(0o7777)
 
 split_brackets = re.compile(r' (?=\()')
 def make_launcher(launch_params: 'LaunchCommand', name: str, metadata: 'Metadata', id_type: str, unique_id: str):
