dc4fcc9bac4beaf9f12531f53a230356b75062e5
Megan Leet <megan.leet@iinet.net.au>
2021-11-23 10:39:20 +1100

    This belongs over here

--- meowlauncher/util/io_utils.py
+++ meowlauncher/util/io_utils.py
@@ -1,7 +1,7 @@
 import os
 import pathlib
-import re
 from typing import Optional
+
 
 def ensure_exist(path: pathlib.Path):
 	path.parent.mkdir(exist_ok=True, parents=True)
@@ -47,24 +47,11 @@ def sanitize_name(s: Optional[str], supersafe: bool=False) -> str:
 		return 'dotdot'
 	return s
 
-_remove_brackety_things_for_filename = re.compile(r'[]([)]')
-_clean_for_filename = re.compile(r'[^A-Za-z0-9_]')
-def make_filename(name: str) -> str:
-	name = name.lower()
-	name = _remove_brackety_things_for_filename.sub('', name)
-	name = _clean_for_filename.sub('-', name)
-	while name.startswith('-'):
-		name = name[1:]
-	if not name:
-		name = 'blank'
-	return name
-
-def pick_new_filename(folder: pathlib.Path, display_name: str, extension: str) -> str:
-	base_filename = make_filename(display_name)
-	filename = base_filename + os.extsep + extension
+def pick_new_path(folder: pathlib.Path, base_filename: str, extension: str) -> pathlib.Path:
+	new_path = folder.joinpath(base_filename + os.extsep + extension)
 
 	i = 2
-	while folder.joinpath(filename).is_file():
-		filename = base_filename + str(i) + os.extsep + extension
+	while new_path.is_file():
+		new_path = new_path.with_stem(base_filename + str(i))
 		i += 1
-	return filename
+	return new_path
