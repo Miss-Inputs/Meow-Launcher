0567e709ec8e8eb51260ebc4971b99d302284100
Megan Leet <megan.leet@iinet.net.au>
2022-11-16 00:17:33 +1100

    MAME machines: Optimize get_image by loading known image paths first

--- meowlauncher/games/mame_common/mame_configuration.py
+++ meowlauncher/games/mame_common/mame_configuration.py
@@ -1,12 +1,18 @@
+from functools import cache
 import os
 import re
-from collections.abc import Mapping, Sequence
+from collections.abc import Mapping, Sequence, Collection
 from pathlib import Path
 
 image_types = {'ico', 'png', 'jpg', 'bmp'}
 
+
+@cache
+def _list_images_in_dir(path: Path) -> 'Collection[Path]':
+	return {file for file in path.iterdir() if file.suffix[1:].lower() in image_types}
+
 class MAMEConfiguration():
-	def __init__(self, core_config_path: Path|None=None, ui_config_path: Path|None=None) -> None:
+	def __init__(self, core_config_path: Path | None=None, ui_config_path: Path | None=None) -> None:
 		if not core_config_path:
 			core_config_path = Path('~/.mame/mame.ini').expanduser()
 		self.core_config = parse_mame_config_file(core_config_path)
@@ -17,14 +23,16 @@ class MAMEConfiguration():
 
 	def get_image(self, config_key: str, machine_or_list_name: str, software_name: str | None=None) -> Path | None:
 		for directory in self.ui_config.get(config_key, ()):
-			basename = Path(directory, machine_or_list_name)
+			directory_path = Path(directory)
+			basename = directory_path / machine_or_list_name
 			if software_name:
-				basename = basename.joinpath(software_name)
+				basename = basename / software_name
 			for ext in image_types:
 				path = basename.with_suffix(os.path.extsep + ext)
-				if path.is_file():
+				if path in _list_images_in_dir(directory_path):
 					return path
 		return None
+
 
 _mame_config_comment = re.compile(r'#.+$')
 _mame_config_line = re.compile(r'^(?P<key>\w+)\s+(?P<value>[^;]+|"[^"]+")$')
