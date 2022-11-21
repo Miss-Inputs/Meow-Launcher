69a87461fd15f97288c4333d8018d9da399a962a
Megan Leet <megan.leet@iinet.net.au>
2022-11-22 01:48:43 +1100

    Docstrings…

--- meowlauncher/launcher.py
+++ meowlauncher/launcher.py
@@ -7,29 +7,33 @@ if TYPE_CHECKING:
 	from .configured_runner import ConfiguredRunner
 
 class Launcher(ABC):
+	"""Base class for Launcher - the actual output of Meow Launcher, which is generally something that launches a game, but may also include sub-games or variations"""
 	def __init__(self, game: 'Game', runner: 'ConfiguredRunner') -> None:
 		self.game = game
 		self.runner = runner
 
 	@property
 	def name(self) -> str:
+		"""Display name of this launcher. By default, the game's name, override if your GameSource has some other way of getting a name"""
 		return self.game.name
 
 	@property
 	@abstractmethod
 	def game_type(self) -> str:
-		pass
+		"""This is written to the ID section of the launcher, and must be unique to your GameSource, as it is used to know what game_id means"""
 	
 	@property
 	@abstractmethod
 	def game_id(self) -> str:
-		pass
+		"""This is written to the ID section of the launcher, and is used to uniquely identify a game"""
+		#Hmm does that mean it should be on Game instead? Or GameSource as a method taking Game?
 
 	@property
 	@abstractmethod
 	def command(self) -> 'LaunchCommand':
-		pass
+		"""The actual command that this launcher executes"""
 
 	def __hash__(self) -> int:
 		return hash((self.game_type, self.game_id))
 		
+__doc__ = Launcher.__doc__
