3d54042a1fb4b0aad6339c44cdc54de93623a1b8
Megan Leet <megan.leet@iinet.net.au>
2022-11-24 00:09:09 +1100

    Whoops

--- meowlauncher/frontend/disambiguate.py
+++ meowlauncher/frontend/disambiguate.py
@@ -234,7 +234,7 @@ def _arcade_system_disambiguate(arcade_system: str | None, name: str) -> str | N
 
 def _platform_type_disambiguate(d: DesktopWithPath) -> str | None:
 	typey = get_field(d.parser, 'Type', id_section_name)
-	return get_field(d.parser, 'Platform') if typey in {'ROMs', 'Arcade'} else typey
+	return get_field(d.parser, 'Platform') if typey in {'ROMs', 'Arcade / standalone machines'} else typey
 		
 def disambiguate_names() -> None:
 	time_started = time.perf_counter()
