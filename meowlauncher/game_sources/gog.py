53857f7a1734f6224d3e7c7c0bcc2eae5572a830
Megan Leet <megan.leet@iinet.net.au>
2022-11-28 21:45:32 +1100

    Refactorino

--- meowlauncher/game_sources/gog.py
+++ meowlauncher/game_sources/gog.py
@@ -40,17 +40,14 @@ def look_in_linux_gog_folder(folder: Path) -> GOGGame | None:
 	return None
 
 def look_in_windows_gog_folder(folder: Path) -> WindowsGOGGame | None:
-	info_file: Path | None = None
-	game_id: str
-	for file in folder.iterdir():
-		if file.name.startswith('goggame-') and file.suffix == '.info':
-			info_file = file
-			game_id = file.name[8:-5]
-			break
-		#Other files that may exist: goggame-<gameid>.dll, goggame-<gameid>.hashdb (Galaxy related?), goggame-<gameid>.ico, goglog.ini, goggame-galaxyFileList.ini, goggame.sdb, unins000.exe, unins000.dat, unins000.msg
+	"""Checks if folder has a goggame-<gameid>.info file in it and returns WindowsGOGGame if so"""
+	info_file = next((file for file in folder.iterdir() if file.stem.startswith('goggame-') and file.suffix == '.info' ), None)
+	#Other files that may exist: goggame-<gameid>.dll, goggame-<gameid>.hashdb (Galaxy related?), goggame-<gameid>.ico (but this is a generic GOG icon), goglog.ini, goggame-galaxyFileList.ini, goggame.sdb, unins000.exe, unins000.dat, unins000.msg, other stuff that user probably actually wants to delete because it sucks
 	if not info_file:
 		#There are actually instances where this isn't here: Bonus content items (Prisoner of Ice UK version, SOTC Floppy version, etc) but I don't really know what to do with those right now to be quite honest, I guess I could look for "Launch <blah>.lnk" and extract which game to launch from there? But that's weird
+		#Or user could have deleted it
 		return None
+	game_id = info_file.stem.removeprefix('goggame-')
 	return WindowsGOGGame(folder, info_file, game_id)
 
 def do_linux_gog_games() -> None:
@@ -97,5 +94,6 @@ def do_gog_games() -> None:
 	do_linux_gog_games()
 
 	#TODO: Should have is_wine_available helper function or whatever
+	#TODO: Actually, shouldn't check that at all - once we do this properly, it could be possible to not have Wine but to run DOS WindowsGOGGames with DOSBox etc
 	if main_config.wine_path.is_file():
 		do_windows_gog_games()
