068b5158c570214b41728cedc815164f3ed7cdfd
Megan Leet <megan.leet@iinet.net.au>
2022-12-08 19:52:50 +1100

    Was going to do more here but then I remembered I'm still tired

--- meowlauncher/runner.py
+++ meowlauncher/runner.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from enum import Enum, auto
 
 from meowlauncher.config_types import RunnerConfigValue
+from meowlauncher.config.config import Config, configoption
 
 class HostPlatform(Enum):
 	"""Platform this runner runs on"""
@@ -11,6 +12,26 @@ class HostPlatform(Enum):
 	Java = auto()
 	Love = auto()
 	HTML = auto()
+
+class BaseRunnerConfig(Config):
+	"""All runners would have this config. Section not defined here, so it is still abstract
+	TODO: Use this once we refactor Emulator and Emulator.configs"""
+	
+	@configoption
+	def gamemode(self) -> bool:
+		"""Run this with gamemoderun"""
+		return False
+	
+	@configoption
+	def mangohud(self) -> bool:
+		"""Run this with MangoHUD"""
+		return False
+
+	@configoption
+	def force_opengl_version(self) -> bool:
+		"""Forces Mesa OpenGL version to 4.3 via environment variable if you need it
+		Is this still needed? I don't know"""
+		return False
 
 class Runner(ABC):
 	"""Base class for a runner (an emulator, compatibility layer, anything that runs a thing). Defines the capabilities/options/etc of the runner, see ConfiguredRunner for the instance with options applied"""
